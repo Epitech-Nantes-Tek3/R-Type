c1b103b8b4448d4d0965d282e6eb9c0f68991472
Bunny <tomduvalmahe44@gmail.com>
2022-09-22 10:11:57 +0200

    Add main for Server and Client
    Add a main for Server
    Add a main for Client

---
+++ Client/main.cpp
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2022
+** R-Type
+** File description:
+** main for the client
+*/
+
+int main()
+{
+    return (0);
+}
---
+++ Server/main.cpp
@@ -0,0 +1,11 @@
+/*
+** EPITECH PROJECT, 2022
+** R-Type
+** File description:
+** main for the server
+*/
+
+int main()
+{
+    return (0);
+}
