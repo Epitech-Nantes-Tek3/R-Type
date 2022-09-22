0caef013d5a789497719ee29e3d9e2d3655eb023
osvegn <thomas.prudhomme@epitech.eu>
2022-09-22 14:08:15 +0200

    add detectCompiler It will detect the compiler to know if it's run on linux or windows #10

---
+++ cmake/DetectCompiler.cmake
@@ -0,0 +1,15 @@
+# Script that sets the COMPILER_TYPE variable according to the current variable
+#
+# Possible values:
+# "msvc" (windows-only)
+# "gcc" (linux, mingw not supported yet)
+
+
+if (CMAKE_COMPILER_IS_GNUCXX)
+  set(COMPILER_TYPE "gcc")
+elseif (MSVC)
+  set(COMPILER_TYPR "msvc")
+else ()
+  set(COMPILER_TYPE "unknown")
+  message(WARNING "Unknown compiler type, compilation may not work as expected")
+endif()
