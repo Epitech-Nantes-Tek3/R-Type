8199aba71667234fa703daca86a492950eaa3ae3
Bunny <tomduvalmahe44@gmail.com>
2022-09-23 15:02:59 +0200

    Delete builds files

--- build/CMakeFiles/r-type_server.dir/Server/main.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/r-type_server.dir/Server/main.cpp.o: \
- /home/Bunny/Documents/Epitech/Teck3/R-Type/R-Type/Server/main.cpp \
- /usr/include/stdc-predef.h
--- build/CMakeFiles/r-type_server.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for r-type_server.
-# This may be replaced when dependencies are built.
