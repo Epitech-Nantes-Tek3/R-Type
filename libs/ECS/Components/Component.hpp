2effe12c202c918b32f434b4be088f26d8601ecf
Bunny <tomduvalmahe44@gmail.com>
2022-09-23 17:31:56 +0200

    Change Component Class for pull request
    Constructor in protected
    
    Add a constructor to had a std::string &name and std::string name
    
    var std::string _name in const

--- libs/ECS/Components/Component.hpp
+++ libs/ECS/Components/Component.hpp
@@ -14,19 +14,23 @@ namespace ecs
     class Component
     {
     public:
-    
-        /// @brief This is the constructor of Component Class
-        Component();
 
         /// @brief This is the destructor of Component Class
-        ~Component();
-
+        virtual ~Component() = default;
+  
         /// @brief this function give the name of the component (itself)
         /// @return the name of itself
-        std::string getName() { return this->_name; };
-
+        const std::string getName() const { return this->_name; };
+  
     protected:
+
+        /// @brief This is the constructor of Component Class
+        Component(const std::string name) : _name(name) {};
+
+        /// @brief This is the constructor of Component Class
+        Component(const std::string &name) : _name(name) {};
+
     private:
-        std::string _name;
+        const std::string _name;
     };
 }
