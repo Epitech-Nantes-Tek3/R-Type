8b22bbfe55a97f6e9bf82f3a6f6cf7bb31919c40
Bunny <tomduvalmahe44@gmail.com>
2022-09-23 16:31:24 +0200

    Add Cmake to create ecs shared library

---
+++ libs/ECS/Components/Component.hpp
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2022
+** R-Type
+** File description:
+** Component
+*/
+
+#pragma once
+
+#include <string>
+
+namespace ecs
+{
+    class Component
+    {
+    public:
+        /// @brief This is the constructor of Component Class
+        Component();
+
+        /// @brief This is the destructor of Component Class
+        ~Component();
+
+        /// @brief this function give the name of the component (itself)
+        /// @return the name of itself
+        std::string getName() { return this->_name; };
+
+    protected:
+    private:
+        std::string _name;
+    };
+}
