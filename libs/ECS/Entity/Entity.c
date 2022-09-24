e2a6c5382fc5eafab6d8eef20814cf1e4b2e66d9
Quentin C. | ZQUEMA <zquema1@gmail.com>
2022-09-24 16:57:14 +0200

    Add a first draft of the class
    THIS IS A DRAFT.
    It contains the class and the functions but they are not documented and tested yet. There are probably some bugs

--- libs/ECS/Entity/Entity.c
+++ libs/ECS/Entity/Entity.c
@@ -4,4 +4,3 @@
 ** File description:
 ** Entity
 */
-
--- libs/ECS/Entity/Entity.hpp
+++ libs/ECS/Entity/Entity.hpp
@@ -7,17 +7,76 @@
 
 #pragma once
 
-/// @file libs/ECS/System.hpp
+#include <concepts>
+#include <cstddef>
+#include <memory>
+#include <typeindex>
+#include <unordered_map>
+
+#include "../Components/Component.hpp"
+
+/// @file libs/ECS/Entity/Entity.hpp
 
 namespace ecs
 {
     class Entity
     {
     public:
-        Entity();
+        /// The id type
+        using Index = std::size_t;
+
+        using ComponentsList = std::unordered_map<std::type_index, std::unique_ptr<Component>>;
+
+        /// @returns The index of this entity.
+        constexpr Index getId() const noexcept { return this->_id; }
+
+        /// Entity index comparison.
+        auto operator<=>(Entity const &other) const { return this->_id <=> other._id; }
+
+        template <std::derived_from<Component> C, typename... Args>
+        Entity &addComponent(C component, Args &&...args)
+        {
+            // if already contains once -> throw ERROR
+
+            this->_componentList[std::type_index(typeid(C))] = std::make_unique<C, Args...>(std::forward<Args>(args...));
+            return this;
+        }
+
+        template <std::derived_from<Component> C>
+        Component &getComponent() const
+        {
+            ComponentsList::iterator it = _componentList.find(std::type_index(typeid(C)));
+            if (it == _componentList.end())
+                throw std::logic_error("attempted to get a non-existent component");
+            return it->second;
+        }
+
+        template <std::derived_from<Component> C>
+        void removeComponent()
+        {
+            ComponentsList::iterator it = _componentList.find(std::type_index(typeid(C)));
+            if (it == _componentList.end())
+                throw std::logic_error("attempted to remove a non-existent component");
+            _componentList.erase(it);
+        }
+
+        template <std::derived_from<Component>... C>
+        bool contains(C... component) const
+        {
+            if (_componentList.find(std::type_index(typeid(component...))) == _componentList.end())
+                return false;
+            return contains(component);
+        }
+
         ~Entity();
 
-    protected:
     private:
+        Index _id;
+
+        ComponentsList _componentList;
+
+        inline constexpr Entity(Index id) : _id(id) {}
+
+        inline constexpr bool contains() { return true;}
     };
 }
