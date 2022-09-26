57b1226cdefde4ca381789aa72a8b7693234c714
osvegn <thomas.prudhomme@epitech.eu>
2022-09-26 23:01:49 +0200

    add SFMLconfig.cmake in cmake folder and update setup.bat #10

--- setup.bat
+++ setup.bat
@@ -1,6 +1,3 @@
 git submodule init
 git submodule update
-vcpkg/bootstrap-vcpkg.bat -disableMetrics
-vcpkg/vcpkg install SFML
-cmake -B build -DCMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake
-cmake --build build
+vcpkg/bootstrap-vcpkg.bat -disableMetrics && vcpkg/vcpkg install SFML && cmake -B build -DCMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake && cmake --build build
