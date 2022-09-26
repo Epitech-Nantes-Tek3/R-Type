08cfda9934c589ad26431a23ad64e8338c48373b
Nathan <nathan.rousseau@epitech.eu>
2022-09-26 14:33:06 +0200

    Add basics main for executable
    Both of the mains print "binary name" and 42 #12

---
+++ Client/main.cpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2022
+** R-Type
+** File description:
+** Project Main
+*/
+
+#include <iostream>
+
+/// @brief Main Function
+/// @param ac The number of arguments
+/// @param av The arguments
+/// @return 0 if sucess, an error code otherwise
+int main(int ac, char **av)
+{
+    (void)ac;
+    (void)av;
+    std::cout << "Client : 42" << std::endl;
+    return 0;
+}
