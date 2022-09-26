31fe09b1d10835f0b15abcaa609245a19a383ab4
osvegn <thomas.prudhomme@epitech.eu>
2022-09-26 22:22:41 +0200

    fix compilation's error Install some dependancies for SFML library. Try another format for the setup.bat file. #10

--- setup.bat
+++ setup.bat
@@ -1,10 +1,6 @@
 git submodule init
 git submodule update
-
-./vcpkg/bootstrap-vcpkg.bat -disableMetrics
-
-./vcpkg/vcpkg install SFML
-
+vcpkg/bootstrap-vcpkg.bat -disableMetrics
+vcpkg/vcpkg install SFML
 cmake -B build -DCMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake
-
 cmake --build build
