00c6f2f28485ee04f25850afb495706a6b2625c4
osvegn <thomas.prudhomme@epitech.eu>
2022-09-26 23:23:19 +0200

    try another syntax on setup.bat #10

--- setup.bat
+++ setup.bat
@@ -1,6 +1,6 @@
 git submodule init
 git submodule update
-call vcpkg/bootstrap-vcpkg.bat -disableMetrics
-call vcpkg/vcpkg install SFML
+call ./vcpkg/bootstrap-vcpkg.bat -disableMetrics
+vcpkg/vcpkg install SFML
 cmake -B build -DCMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake
 cmake --build build
