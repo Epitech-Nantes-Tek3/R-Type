2ae595a06c815df8810d05ad6347c9bdd1bc2c20
Lucas Tesnier <lucas.tesnier@epitech.eu>
2022-09-27 09:55:14 +0200

    Implement the communicator library architecture #26

---
+++ libs/Communicator/entrypoint.cpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2022
+** Project
+** File description:
+** entrypoint
+*/
+
+#include "entrypoint.hpp"
+#include <iostream>
+
+/// \file libs/Communicator/entrypoint.cpp
+
+void entrypoint(void)
+{
+    std::cout << "Welcome to the communicator library" << std::endl;
+    return;
+}
---
+++ libs/Communicator/include/entrypoint.hpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2022
+** Project
+** File description:
+** entrypoint
+*/
+
+#ifndef ENTRYPOINT_HPP_
+    #define ENTRYPOINT_HPP_
+
+/// \brief The first function of the library, will be erased in futur
+void entrypoint(void);
+
+#endif /* !ENTRYPOINT_HPP_ */
