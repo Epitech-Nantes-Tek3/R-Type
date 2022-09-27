a09749483bb8664dd8fcb7448c8096a43625a599
unknown <nathan.rousseau@epitech.eu>
2022-09-27 18:20:58 +0200

    Update setup.bat
    The script can be executed just after cloning the repository #10

--- setup.bat
+++ setup.bat
@@ -1,6 +1,19 @@
-git submodule init
-git submodule update
-call ./vcpkg/bootstrap-vcpkg.bat -disableMetrics
-./vcpkg/vcpkg install SFML
-cmake -B build -DCMAKE_TOOLCHAIN_FILE=./vcpkg/scripts/buildsystems/vcpkg.cmake
-cmake --build build
+mkdir build
+cd build
+
+echo "Build Folder created"
+@pause
+
+conan install .. --build=missing  -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True
+cd ..
+cmake -B ./build -DCMAKE_BUILD_TYPE=Release
+cmake --build ./build
+
+echo "Compilation Done"
+@pause
+
+copy .\Debug\r-type_client.exe .
+copy .\Debug\r-type_server.exe .
+
+echo "Executables created"
+@pause
