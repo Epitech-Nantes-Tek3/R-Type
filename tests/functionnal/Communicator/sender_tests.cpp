3fea76e6321441c07bdd01c698a6d4a0886e348a
Lucas Tesnier <lucas.tesnier@epitech.eu>
2022-09-27 18:38:06 +0200

    Implement the sender class and the associated functionnal testing #24

---
+++ tests/functionnal/Communicator/sender_tests.cpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2022
+** Project
+** File description:
+** sender_tests
+*/
+
+#include "Communicator/Sender.hpp"
+
+using namespace communicator_lib;
+
+int main(int ac, char **av)
+{
+    Sender send = Sender();
+
+    (void) ac;
+    (void) av;
+    return 0;
+}
