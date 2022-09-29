baf4e4ef1db61f5c869e6e6bb3b80b5cf1c1406e
osvegn <thomas.prudhomme@epitech.eu>
2022-09-29 21:23:21 +0200

    Create Events class with namespaces #32

---
+++ Client/SFML_wrapper/Events/Events.cpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2022
+** rtype
+** File description:
+** Events
+*/
+
+#include "Events.hpp"
+
+using namespace Client::SFML_Wrapper;
+
+Events::Events()
+{
+}
+
+Events::~Events()
+{
+}
