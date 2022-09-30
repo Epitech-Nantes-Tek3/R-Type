3b3fcf08f468efa5027fa532730d97535432a8e4
Bunny <tomduvalmahe44@gmail.com>
2022-09-30 13:34:38 +0200

    Add Resource Class
    Add destructor public and constructor in protected area

---
+++ libs/ECS/Resources/Resources.hpp
@@ -0,0 +1,27 @@
+/*
+** EPITECH PROJECT, 2022
+** R-Type
+** File description:
+** Resources
+*/
+
+#pragma once RESOUCES_HPP
+
+
+namespace ecs
+{
+    /// @brief This is the Resource Class for ECS
+    /// The base Resource type, all Resources must inherit from Resource.
+    class Resource {
+      public:
+        /// @brief This is the destructor of Resource Class
+        virtual ~Resource() = default;
+
+      protected:
+        /// @brief This is the constructor of Resource Class
+        /// Because it can be use only by things that eritate by it
+        Resource() = default;
+    };
+} // namespace ecs
+
+
