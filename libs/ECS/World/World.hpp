7ea57fc2411b6068d03b76f0409fdba20ff93571
Bunny <tomduvalmahe44@gmail.com>
2022-09-30 14:15:07 +0200

    Add getResource()
    This function can get a resource in the World

--- libs/ECS/World/World.hpp
+++ libs/ECS/World/World.hpp
@@ -19,6 +19,17 @@ namespace ecs
         World();
         ~World();
 
+        /// @brief This function can get a Resource in the World
+        /// @tparam C Search Resource
+        /// @throw std::logic_error Throw an error if the Resource does not exists
+        /// @return The Resource choosen
+        template <std::derived_from<Resource> C> C &getResource() const
+        {
+            if (_resourceList.count(typeid(C)) == 0)
+                throw std::logic_error("attempted to get a non-existent Resource");
+            return static_cast<C &>(*(_resourceList.at(typeid(C)).get()));
+        }
+
         /// @brief Remove a Resource of the world
         /// @tparam C The choosen Resource to remove
         /// @throw std::logic_error Throw an error if the Resource does not exists
