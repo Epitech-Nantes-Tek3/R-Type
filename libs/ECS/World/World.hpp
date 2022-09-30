edc98563e1b642f6b52b9a4b830305f064b580f8
Bunny <tomduvalmahe44@gmail.com>
2022-09-30 14:04:23 +0200

    Add ResourceList in World Class #39

--- libs/ECS/World/World.hpp
+++ libs/ECS/World/World.hpp
@@ -7,14 +7,22 @@
 
 #pragma once
 
+#include "Resources/Resources.hpp"
+
 namespace ecs
 {
     class World {
       public:
+        using ResourcesList = std::unordered_map<std::type_index, std::unique_ptr<Resource>>;
+
         World();
         ~World();
 
       protected:
+      
       private:
+
+        /// @brief List of Resources of the World Class
+        ResourcesList _resourceList;
     };
 } // namespace ecs
