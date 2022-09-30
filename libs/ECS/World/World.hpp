77cfab0179f92df56d2743c84e161fe3c49b260a
Bunny <tomduvalmahe44@gmail.com>
2022-09-30 14:05:47 +0200

    Add comment ResourceList in World Class #39

--- libs/ECS/World/World.hpp
+++ libs/ECS/World/World.hpp
@@ -13,13 +13,14 @@ namespace ecs
 {
     class World {
       public:
+        /// @brief This is the tab of Resource of the World
         using ResourcesList = std::unordered_map<std::type_index, std::unique_ptr<Resource>>;
 
         World();
         ~World();
 
       protected:
-      
+
       private:
 
         /// @brief List of Resources of the World Class
