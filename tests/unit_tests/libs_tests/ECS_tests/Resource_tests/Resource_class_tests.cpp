3bfd1873954cb8f675ce5834cbbbf4ef1732196f
Bunny <tomduvalmahe44@gmail.com>
2022-09-30 15:19:05 +0200

    Add Test for Resource's function #48
    Add test for contains

--- tests/unit_tests/libs_tests/ECS_tests/Resource_tests/Resource_class_tests.cpp
+++ tests/unit_tests/libs_tests/ECS_tests/Resource_tests/Resource_class_tests.cpp
@@ -13,31 +13,36 @@
 class Timer : public ecs::Resource {
   public:
     int _timer;
-}
+
+    Timer(const int x) : _timer(x) {};
+    Timer(Timer &old) : _timer(old._timer) {};
+};
 
 class Texture : public ecs::Resource {
   public:
     std::filesystem::path _path;
-}
+
+    Texture(const std::filesystem::path path) : _path(path) {};
+};
 
 Test(Resource, add_and_get_resource_in_the_World)
 {
     ecs::World *world = new ecs::World();
 
     world->addResource<Timer>(1);
-    class Timer &timer = resource->getComponent<Timer>();
+    class Timer &timer = world->getResource<Timer>();
     cr_assert_eq(timer._timer, 1);
 
     delete world;
 }
 
-/// @brief Create an Resource and get a non-existing component
+/// @brief Create an Resource and get a non-existing Resource
 Test(Resource, get_non_existant_resource_in_the_World)
 {
     ecs::World *world = new ecs::World();
 
     try {
-        class Timer &pos = world->getComponent<Timer>();
+        class Timer &pos = world->getResource<Timer>();
         (void)pos;
     } catch (std::logic_error &err) {
         cr_assert_str_eq(err.what(), "attempted to get a non-existent Resource");
@@ -66,7 +71,7 @@ Test(Resource, remove_a_resource)
 /// @brief Try to remove a non-existing Resource from an World
 Test(Resource, remove_a_non_existant_Resource)
 {
-    ecs::World *world = new ecs::World(1);
+    ecs::World *world = new ecs::World();
 
     try {
         world->removeResource<Timer>();
