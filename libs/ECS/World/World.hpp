0581164efabef229157a28c6334470e09b47fa6e
Quentin C. | ZQUEMA <zquema1@gmail.com>
2022-10-01 14:07:00 +0200

    Merge with the ressource class

--- libs/ECS/World/World.hpp
+++ libs/ECS/World/World.hpp
@@ -28,8 +28,12 @@ namespace ecs
         /// @brief This is the map of Entities in the World
         using EntitiesList = std::map<Index, std::unique_ptr<Entity>>;
 
-        /// @brief This is the tab of Resource of the World
+        /// @brief This is the map of Resource of the World
         using ResourcesList = std::unordered_map<std::type_index, std::unique_ptr<Resource>>;
+
+        ///@brief Construct a new World object
+        ///@param id Id of the searched Entity
+        inline World(Index id) : _id(id), _entitiesList(), _nextEntityId(0){};
 
         ///@brief Get the Id object
         ///@return Index
@@ -50,32 +54,6 @@ namespace ecs
         ///@throw std::logic_error Throw an error if the entity does not exists
         void removeEntity(Index id);
 
-        ///@brief Construct a new World object
-        ///@param id Id of the searched Entity
-        inline World(Index id) : _id(id), _entitiesList(), _nextEntityId(0){};
-
-        /// @brief This function can get a Resource in the World
-        /// @tparam C Search Resource
-        /// @throw std::logic_error Throw an error if the Resource does not exists
-        /// @return The Resource choosen
-        template <std::derived_from<Resource> C> C &getResource() const
-        {
-            if (_resourceList.count(typeid(C)) == 0)
-                throw std::logic_error("attempted to get a non-existent Resource");
-            return static_cast<C &>(*(_resourceList.at(typeid(C)).get()));
-        }
-
-        /// @brief Remove a Resource of the world
-        /// @tparam C The choosen Resource to remove
-        /// @throw std::logic_error Throw an error if the Resource does not exists
-        template <std::derived_from<Resource> C> void removeResource()
-        {
-            ResourcesList::iterator it = _resourceList.find(typeid(C));
-            if (it == _resourceList.end())
-                throw std::logic_error("attempted to remove a non-existent Resource");
-            _resourceList.erase(it);
-        }
-
         /// @brief This function can add a Resource to the World
         /// @tparam C Type of the Resource
         /// @tparam ...Args This allows to send multiple arguments (args)
@@ -86,8 +64,30 @@ namespace ecs
         {
             if (contains<C>())
                 throw std::logic_error("attempted to add Resource that already exists");
-            this->_resourceList[std::type_index(typeid(C))] = std::make_unique<C>(std::forward<Args>(args)...);
+            this->_resourcesList[std::type_index(typeid(C))] = std::make_unique<C>(std::forward<Args>(args)...);
             return *this;
+        }
+
+        /// @brief This function can get a Resource in the World
+        /// @tparam C Search Resource
+        /// @throw std::logic_error Throw an error if the Resource does not exists
+        /// @return The Resource choosen
+        template <std::derived_from<Resource> C> C &getResource() const
+        {
+            if (_resourcesList.count(typeid(C)) == 0)
+                throw std::logic_error("attempted to get a non-existent Resource");
+            return static_cast<C &>(*(_resourcesList.at(typeid(C)).get()));
+        }
+
+        /// @brief Remove a Resource of the world
+        /// @tparam C The choosen Resource to remove
+        /// @throw std::logic_error Throw an error if the Resource does not exists
+        template <std::derived_from<Resource> C> void removeResource()
+        {
+            ResourcesList::iterator it = _resourcesList.find(typeid(C));
+            if (it == _resourcesList.end())
+                throw std::logic_error("attempted to remove a non-existent Resource");
+            _resourcesList.erase(it);
         }
 
         /// @brief This function will check if a group of Resources types (at least one Resource type) is in an World
@@ -96,20 +96,16 @@ namespace ecs
         /// @return True if the group of Resource types is contained in the World. Otherwise False
         template <std::derived_from<Resource> C1, std::derived_from<Resource>... C2> bool contains() const
         {
-            if (_resourceList.count(typeid(C1)) == 0)
+            if (_resourcesList.count(typeid(C1)) == 0)
                 return false;
             return contains<C2...>();
         }
+
         ///@brief Destroy the World object
         ~World() = default;
 
       protected:
       private:
-        /// @brief This is the function which is called when none Resources types left
-        /// @tparam ...C The last research Resource
-        /// @return True
-        template <std::derived_from<Resource>... C>
-        requires(sizeof...(C) == 0) bool contains() const { return true; }
         ///@brief World's id
         Index _id;
 
@@ -120,6 +116,12 @@ namespace ecs
         Index _nextEntityId;
 
         /// @brief List of Resources of the World Class
-        ResourcesList _resourceList;
+        ResourcesList _resourcesList;
+
+        /// @brief This is the function which is called when none Resources types left
+        /// @tparam ...C The last research Resource
+        /// @return True
+        template <std::derived_from<Resource>... C>
+        requires(sizeof...(C) == 0) bool contains() const { return true; }
     };
 } // namespace ecs
