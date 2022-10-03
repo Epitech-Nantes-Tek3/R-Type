b4f84e69e4179cd5f08c0ae480767f6e84f3183a
Nathan <nathan.rousseau@epitech.eu>
2022-10-03 22:20:06 +0200

    Add a new component
    Position Component added #62

---
+++ libs/ECS/Components/PositionComponent.hpp
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2022
+** Project
+** File description:
+** PositionComponent
+*/
+
+#ifndef POSITIONCOMPONENT_HPP_
+#define POSITIONCOMPONENT_HPP_
+
+#include "Component.hpp"
+
+namespace ecs
+{
+    /// @brief The Position component, almost used for every entities
+    class Position : public Component {
+      public:
+        /// @brief The abscissa of the Position
+        int x;
+        /// @brief The ordinate of the Position
+        int y;
+
+        /// @brief Constructor of the Position component
+        /// @param X the abscissa
+        /// @param Y the ordinate
+        Position(const int &X, const int &Y) : x(X), y(Y){};
+        /// @brief Copy Constructor of the Position Component
+        /// @param old The copy of the Position Component
+        Position(const Position &old) : x(old.x), y(old.y) {};
+        /// @brief Default Destructor
+        ~Position() = default;
+    };
+} // namespace ecs
+
+#endif /* !POSITIONCOMPONENT_HPP_ */
