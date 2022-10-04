a0c0de74dfee434b7fc07003ea19a0b4be92e50f
Nathan <nathan.rousseau@epitech.eu>
2022-10-04 10:18:19 +0200

    Add new Component
    Damage Component added #62

---
+++ libs/ECS/Components/DamageComponent.hpp
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2022
+** Project
+** File description:
+** DamageComponent
+*/
+
+#ifndef DAMAGECOMPONENT_HPP_
+#define DAMAGECOMPONENT_HPP_
+
+#include "Component.hpp"
+
+namespace ecs
+{
+    /// @brief The Damage class, reducing the entity's life
+    class Damage : public Component{
+      public:
+        /// @brief The number of damage, to be substract to the Life Point
+        unsigned int damagePoint;
+
+        /// @brief Constructor of the Damage Component
+        /// @param newDamage the number of damage
+        Damage(unsigned int newDamage) : damagePoint(newDamage) {};
+        /// @brief Copy Constructor of the Damage Component
+        /// @param old the copy of the Damage component
+        Damage(const Damage &old) : damagePoint(old.damagePoint) {};
+        /// @brief Default Destructor
+        ~Damage() = default;
+    };
+} // namespace ecs
+
+#endif /* !DAMAGECOMPONENT_HPP_ */
