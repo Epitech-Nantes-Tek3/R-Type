8241046630557f7aecc2fa67272a3488a8413bb2
osvegn <thomas.prudhomme@epitech.eu>
2022-10-04 19:36:50 +0200

    Update build optimisation #59

--- build_tests.sh
+++ build_tests.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-mkdir -p build; cd build && conan install .. --build=missing -s compiler.libcxx=libstdc++11 -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True && cmake -DCMAKE_BUILD_TYPE=Debug -DTESTING=On .. && cmake --build . -- -j 3 && ctest --output-on-failure
+mkdir -p build; cd build && conan install .. --build=missing -s compiler.libcxx=libstdc++11 -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True && cmake -DCMAKE_BUILD_TYPE=Debug -DTESTING=On .. && cmake --build . -- -j && ctest --output-on-failure
