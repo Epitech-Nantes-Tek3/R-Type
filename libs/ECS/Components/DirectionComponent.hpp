4612bbbc586afce14a35f396c65042ac7cc704ea
Nathan <nathan.rousseau@epitech.eu>
2022-10-04 16:43:59 +0200

    Changing movement component by direction component #62

---
+++ libs/ECS/Components/DirectionComponent.hpp
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2022
+** Project
+** File description:
+** DirectionComponent
+*/
+
+#ifndef DIRECTIONCOMPONENT_HPP_
+#define DIRECTIONCOMPONENT_HPP_
+
+#include "Component.hpp"
+
+namespace ecs
+{
+    /// @brief The Direction component, used for mobiles entities
+    class Direction : public Component {
+      public:
+        /// @brief The destination (abscissa) of the Direction
+        int x;
+        /// @brief The destination (ordinate) of the Direction
+        int y;
+
+        /// @brief Constructor of the Direction component
+        /// @param X the destination (abscissa)
+        /// @param Y the destination (ordinate)
+        Direction(const int &X, const int &Y) : x(X), y(Y){};
+        /// @brief Copy Constructor of the Direction Component
+        /// @param old The copy of the Direction Component
+        Direction(const Direction &old) : x(old.x), y(old.y) {};
+        /// @brief Default Destructor
+        ~Direction() = default;
+    };
+} // namespace ecs
+
+#endif /* !DirectionCOMPONENT_HPP_ */
