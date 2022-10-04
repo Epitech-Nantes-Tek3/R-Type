c01783d8b761dae57a8ce5982b1b753d45a27596
Nathan <nathan.rousseau@epitech.eu>
2022-10-04 16:40:13 +0200

    Empty the Controlable element to make it compliant with the others components of the same type #62

--- libs/ECS/Components/ControlableComponent.hpp
+++ libs/ECS/Components/ControlableComponent.hpp
@@ -13,21 +13,7 @@
 namespace ecs
 {
     /// @brief The controlable class, defining if an entity is affected by the player's input
-    class Controlable : public Component {
-      public:
-        /// @brief True if the corresponding entity is linked to the player
-        bool IsControlable;
-
-        /// @brief Constructor of the Controlable Component
-        /// @param control true if the linked entity will take player's input
-        Controlable(bool control) : IsControlable(control){};
-        /// @brief Copy constructor of the Controlable Component
-        /// @param old  the copy of the Controlable Component
-        Controlable(const Controlable &old) : IsControlable(old.IsControlable){};
-
-        /// @brief Default Destructor
-        ~Controlable() = default;
-    };
+    class Controlable : public Component {};
 } // namespace ecs
 
 #endif /* !CONTROLABLECOMPONENT_HPP_ */
