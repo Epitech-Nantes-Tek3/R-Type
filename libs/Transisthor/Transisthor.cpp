814a4838387fe2f92146eb8970c8f75b29d675a9
Lucas Tesnier <lucas.tesnier@epitech.eu>
2022-10-04 16:58:58 +0200

    Add transisthor inside the project architecture #66

---
+++ libs/Transisthor/Transisthor.cpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2022
+** Project
+** File description:
+** Transisthor
+*/
+
+/// @file libs/Transisthor/Transisthor.cpp
+
+#include "Transisthor.hpp"
+#include "Error/Error.hpp"
+
+using namespace transisthor_lib;
+using namespace ecs;
+using namespace communicator_lib;
+using namespace error_lib;
+
+Transisthor::Transisthor()
+{
+}
+
+Transisthor::~Transisthor()
+{
+}
---
+++ libs/Transisthor/Transisthor.hpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2022
+** Project
+** File description:
+** Transisthor
+*/
+
+/// @file libs/Transisthor/Transisthor.hpp
+
+#ifndef TRANSISTHOR_HPP_
+#define TRANSISTHOR_HPP_
+
+#include "Communicator/Communicator.hpp"
+#include "World/World.hpp"
+
+namespace transisthor_lib
+{
+    class Transisthor {
+      public:
+        Transisthor();
+        ~Transisthor();
+
+      protected:
+      private:
+    };
+} // namespace transisthor_lib
+
+#endif /* !TRANSISTHOR_HPP_ */
