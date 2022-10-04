b043354fd0a3a6c03bfb8b5b5b162bdfafa97e36
Nathan <nathan.rousseau@epitech.eu>
2022-10-04 14:22:40 +0200

    Add New component
    Weight Component Added #62

---
+++ libs/ECS/Components/WeightComponent.hpp
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2022
+** Project
+** File description:
+** WeightComponent
+*/
+
+#ifndef WEIGHTCOMPONENT_HPP_
+#define WEIGHTCOMPONENT_HPP_
+
+#include "Component.hpp"
+
+namespace ecs
+{
+    /// @brief The Weight class, almost used for everything, used in gravity system
+    class Weight : public Component{
+      public:
+        /// @brief weight of the entity
+        unsigned short weight;
+
+        /// @brief Constructor of the Weight Component
+        /// @param newWeight the weight
+        Weight(unsigned short newWeight) : weight(newWeight) {};
+        /// @brief Copy Constructor of the Weight Component
+        /// @param old the copy of the Weight component
+        Weight(const Weight &old) : weight(old.weight) {};
+        /// @brief Default Destructor
+        ~Weight() = default;
+    };
+} // namespace ecs
+
+#endif /* !WEIGHTCOMPONENT_HPP_ */
