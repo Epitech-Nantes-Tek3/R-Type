3ab256aefc7f86a4aa527f8de7caac6f6ce46c66
Nathan <nathan.rousseau@epitech.eu>
2022-10-04 10:51:53 +0200

    Add new Component
    Lifetime Component added #62

--- libs/ECS/Components/ShootingFrequencyComponent.hpp
+++ libs/ECS/Components/ShootingFrequencyComponent.hpp
@@ -19,13 +19,13 @@ namespace ecs
     class ShootingFrequency : public Component{
       public:
         /// @brief The delay between two shoots
-        double frequency;
-        /// @brief Store the values of now, will be usefull in the corresponding system
+        duration<double> frequency;
+        /// @brief Store the current time, will be usefull in the corresponding system
         system_clock::time_point now = system_clock::now();
 
         /// @brief Constructor of the ShootingFrequency Component
         /// @param newfrequency the shooting's delay
-        ShootingFrequency(double newFrequency) : frequency(newFrequency) {};
+        ShootingFrequency(duration<double> newFrequency) : frequency(newFrequency) {};
         /// @brief Copy Constructor of the ShootingFrequency Component
         /// @param old the copy of the ShootingFrequency Component
         ShootingFrequency(const ShootingFrequency &old) : frequency(old.frequency) {};
