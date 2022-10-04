6ba0849f6e73d5201a4d15276825af6a8b9fa1ad
Nathan <nathan.rousseau@epitech.eu>
2022-10-04 16:51:24 +0200

    e

--- libs/ECS/Components/VelocityComponent.hpp
+++ libs/ECS/Components/VelocityComponent.hpp
@@ -15,15 +15,18 @@ namespace ecs
     /// @brief The velocity class, multiplying the movement of the entity
     class Velocity : public Component{
       public:
-        /// @brief The multiplier of the movement (between 0.1 and 10)
-        double multiplier;
+        /// @brief The multiplier of the abscissa movement (between 0.1 and 10)
+        double multiplierAbscissa;
+        /// @brief The multiplier of the ordinate movement (between 0.1 and 10)
+        double multiplierOrdinate;
 
         /// @brief Constructor of the Velocity Component
-        /// @param newMultiplier the movement's multiplier
-        Velocity(double newMultiplier) : multiplier(newMultiplier) {};
+        /// @param newMultiplierX the movement's multiplier of abscissa
+        /// @param newMultiplierY the movement's multiplier of ordinate
+        Velocity(double newMultiplierX, double newMultiplierY) : multiplierAbscissa(newMultiplierX), multiplierOrdinate(newMultiplierY) {};
         /// @brief Copy Constructor of the Velocity Component
         /// @param old the copy of the Velocity Component
-        Velocity(const Velocity &old) : multiplier(old.multiplier) {};
+        Velocity(const Velocity &old) : multiplierAbscissa(old.multiplierAbscissa), multiplierOrdinate(old.multiplierOrdinate) {};
         /// @brief Default Destructor
         ~Velocity() = default;
     };
