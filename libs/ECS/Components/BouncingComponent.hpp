694b19d68d4bb189e343a398c111fdedee1d9645
Nathan <nathan.rousseau@epitech.eu>
2022-10-04 14:09:43 +0200

    Add New component
    Bouncing Component Added #62

---
+++ libs/ECS/Components/BouncingComponent.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2022
+** Project
+** File description:
+** BouncingComponent
+*/
+
+#ifndef BOUNCINGCOMPONENT_HPP_
+#define BOUNCINGCOMPONENT_HPP_
+
+#include "Component.hpp"
+
+namespace ecs
+{
+    /// @brief The Bouncing component, to bounce projectiles after hitting one enemy
+    class Bouncing : public Component {};
+} // namespace ecs
+
+#endif /* !BOUNCINGCOMPONENT_HPP_ */
