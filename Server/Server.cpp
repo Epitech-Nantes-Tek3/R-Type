ca12bcc4ec47f65dbd16b16a5d0d817ed0d21771
Lucas Tesnier <lucas.tesnier@epitech.eu>
2022-10-06 16:18:24 +0200

    Implement basic server class #67

---
+++ Server/Server.cpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2022
+** Project
+** File description:
+** Server
+*/
+
+/// @file Server/Server.cpp
+
+#include "Server.hpp"
+#include "Error/Error.hpp"
+
+using namespace server_data;
+using namespace error_lib;
+
+Server::Server()
+{
+}
+
---
+++ Server/Server.hpp
@@ -0,0 +1,25 @@
+/*
+** EPITECH PROJECT, 2022
+** Project
+** File description:
+** Server
+*/
+
+/// @file Server/Server.hpp
+
+#ifndef SERVER_HPP_
+#define SERVER_HPP_
+
+namespace server_data
+{
+    /// @brief Main class of the server part. Hold room process.
+    class Server {
+      public:
+        /// @brief Construct a new server object
+        Server();
+        /// @brief Destroy the Server object
+        ~Server() = default;
+    };
+} // namespace server_data
+
+#endif /* !SERVER_HPP_ */
