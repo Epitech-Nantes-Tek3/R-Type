da23f7c3dbba97167b815056c4fac040e4944bf5
Bunny <tomduvalmahe44@gmail.com>
2022-10-06 15:12:11 +0200

    Add Tests for Movement System

---
+++ tests/unit_tests/libs_tests/ECS_tests/Systems_tests/Velocity_System_tests.cpp
@@ -0,0 +1,39 @@
+/*
+** EPITECH PROJECT, 2022
+** R-Type
+** File description:
+** Velocity_System_tests
+*/
+
+#include <World/World.hpp>
+#include <Entity/Entity.hpp>
+#include <GameComponents/PositionComponent.hpp>
+#include <GameComponents/VelocityComponent.hpp>
+#include <GameComponents/SizeComponent.hpp>
+#include <GameSystems/MouvementSystem.hpp>
+#include <algorithm>
+#include <iostream>
+#include <criterion/criterion.h>
+
+using namespace ecs;
+
+Test(Velocity_System, moove_a_position)
+{
+    World world(1);
+
+    std::size_t id = world.addEntity().addComponent<Position>(10, 10).addComponent<Velocity>(1, 1).getId();
+
+    world.addSystem<Movement>();
+
+    Position pos1 = world.getEntity(id).getComponent<Position>();
+
+    cr_assert_eq(10, pos1.x);
+    cr_assert_eq(10, pos1.y);
+
+    world.runSystems();
+
+    pos1 = world.getEntity(id).getComponent<Position>();
+
+    cr_assert_eq(11, pos1.x);
+    cr_assert_eq(11, pos1.y);
+}
