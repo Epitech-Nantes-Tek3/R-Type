d8f6cec569875b9f61b8856b5981e362d375689f
Lucas Tesnier <lucas.tesnier@epitech.eu>
2022-10-06 16:30:44 +0200

    Implement basic room class #67

---
+++ Server/Room.cpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2022
+** Project
+** File description:
+** Room
+*/
+
+/// @file Server/Room.cpp
+
+#include "Room.hpp"
+#include "Error/Error.hpp"
+
+using namespace server_data;
+using namespace error_lib;
+
+Room::Room()
+{
+}
---
+++ Server/Room.hpp
@@ -0,0 +1,28 @@
+/*
+** EPITECH PROJECT, 2022
+** Project
+** File description:
+** Room
+*/
+
+/// @file Server/Room.hpp
+
+#ifndef ROOM_HPP_
+#define ROOM_HPP_
+
+namespace server_data
+{
+    /// @brief Place where the game run (Like a lobby). Hold all libraries instance.
+    class Room {
+      public:
+        /// @brief Construct a new Room object
+        Room();
+        /// @brief Destroy the Room object
+        ~Room() = default;
+
+      protected:
+      private:
+    };
+} // namespace server_data
+
+#endif /* !ROOM_HPP_ */
