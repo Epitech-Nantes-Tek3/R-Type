0f6baf185827a6504a7dce843e12d94e3e15d1ef
Bunny <tomduvalmahe44@gmail.com>
2022-10-07 11:33:52 +0200

    Add Projectile Ally Component #62

---
+++ libs/GameComponents/ProjectileAllyComponent.hpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2022
+** R-Type
+** File description:
+** ProjectileAllyComponent
+*/
+
+#ifndef PROJECTILEALLYCOMPONENT_HPP_
+#define PROJECTILEALLYCOMPONENT_HPP_
+
+#include <Component/Component.hpp>
+
+namespace ecs
+{
+    /// @brief This Component specifies if an entity is an Ally Projectile
+    class ProjectileAllyComponent {};
+} // namespace ecs
+#endif /* !PROJECTILEALLYCOMPONENT_HPP_ */
