c15cfc2afc7b0fcde37afd29c216868d435b297f
Bunny <tomduvalmahe44@gmail.com>
2022-10-07 11:30:56 +0200

    Add EnemyProjectileComponent #62

---
+++ libs/GameComponents/ProjectileEnemyComponent.hpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2022
+** 1
+** File description:
+** ProjectileEnemyComponent
+*/
+
+#ifndef PROJECTILEENEMYCOMPONENT_HPP_
+#define PROJECTILEENEMYCOMPONENT_HPP_
+
+#include <Component/Component.hpp>
+
+namespace ecs
+{
+    /// @brief This Component specifies if an entity is an Enemy Projectile
+    class ProjectileEnemyComponent {};
+} // namespace ecs
+#endif /* !PROJECTILEENEMYCOMPONENT_HPP_ */
