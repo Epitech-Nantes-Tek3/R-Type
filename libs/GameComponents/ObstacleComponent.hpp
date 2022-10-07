9f89377ce9cc986f3e1b9bc763826d8953307c49
Bunny <tomduvalmahe44@gmail.com>
2022-10-07 15:53:11 +0200

    Add Obstacle Component  #62

---
+++ libs/GameComponents/ObstacleComponent.hpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2022
+** R-Type
+** File description:
+** ObstacleComponent
+*/
+
+#ifndef OBSTACLECOMPONENT_HPP_
+#define OBSTACLECOMPONENT_HPP_
+
+namespace ecs
+{
+   /// @brief This Component specifies if an entity is an Obstacle
+   class Obstacle {}; 
+} // namespace ecs
+
+
+#endif /* !OBSTACLECOMPONENT_HPP_ */
