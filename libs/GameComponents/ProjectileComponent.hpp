1aea4c19eac91f0f4edab3b37d17fbd9f17431f0
Bunny <tomduvalmahe44@gmail.com>
2022-10-07 11:34:00 +0200

    Add Projectile Component #62

---
+++ libs/GameComponents/ProjectileComponent.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2022
+** R-Type
+** File description:
+** ProjectileComponent
+*/
+
+#ifndef PROJECTILECOMPONENT_HPP_
+#define PROJECTILECOMPONENT_HPP_
+
+#include <Component/Component.hpp>
+
+namespace ecs
+{
+   /// @brief This Component specifies if an entity is a Projectile
+   class ProjectileComponent {
+}; 
+} // namespace ecs
+
+#endif /* !PROJECTILECOMPONENT_HPP_ */
