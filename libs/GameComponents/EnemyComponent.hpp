39cdf0a02340858a58ac07e356e70d53e7f00a9e
Bunny <tomduvalmahe44@gmail.com>
2022-10-07 11:33:17 +0200

    Add EnemyComponent #62

---
+++ libs/GameComponents/EnemyComponent.hpp
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2022
+** R-Type
+** File description:
+** EnemyComponent
+*/
+
+#ifndef ENEMYCOMPONENT_HPP_
+#define ENEMYCOMPONENT_HPP_
+
+#include "Component/Component.hpp"
+
+namespace ecs
+{
+    /// @brief This Component specifies if an entity is a player
+    class EnemyComponent {
+    };
+} // namespace ecs
+
+#endif /* !ENEMYCOMPONENT_HPP_ */
+
