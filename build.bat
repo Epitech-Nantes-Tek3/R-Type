9d5963804f33cd64fc75029b521cce090997e3cb
osvegn <thomas.prudhomme@epitech.eu>
2022-10-08 01:43:04 +0200

    Update build.bat to test windows compilation #32

--- build.bat
+++ build.bat
@@ -2,7 +2,6 @@ mkdir build
 cd build
 
 echo "Build Folder created"
-@pause
 
 conan install .. --build=missing  -c tools.system.package_manager:mode=install -c tools.system.package_manager:sudo=True
 cd ..
@@ -10,10 +9,8 @@ cmake -B ./build -DCMAKE_BUILD_TYPE=Release
 cmake --build ./build -j
 
 echo "Compilation Done"
-@pause
 
 copy .\Debug\r-type_client.exe .
 copy .\Debug\r-type_server.exe .
 
 echo "Executables created"
-@pause
