2eddcd8c3b64286b8c8a29aee23eb8591cbe16fc
osvegn <osvegn@users.noreply.github.com>
2022-10-10 06:14:31 +0000

    Deploying to gh-pages from @ R-Type-Epitech-Nantes/R-Type@87d97a20f59214a0d114b64b69efc0233185a19d 🚀

---
+++ navtreedata.js
@@ -0,0 +1,44 @@
+/*
+@licstart  The following is the entire license notice for the
+JavaScript code in this file.
+
+Copyright (C) 1997-2019 by Dimitri van Heesch
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of version 2 of the GNU General Public License as published by
+the Free Software Foundation
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with this program; if not, write to the Free Software Foundation, Inc.,
+51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+@licend  The above is the entire license notice
+for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "Zaaaaaaappppy", "index.html", [
+    [ "R-Type", "index.html#autotoc_md0", [
+      [ "Prerequisites", "index.html#autotoc_md1", null ],
+      [ "Build the program", "index.html#autotoc_md2", null ],
+      [ "Testing program", "index.html#autotoc_md3", null ],
+      [ "Supported Platform", "index.html#autotoc_md4", null ],
+      [ "Usefulls Links", "index.html#autotoc_md5", null ],
+      [ "Thank for reading", "index.html#autotoc_md6", null ],
+      [ "Authors", "index.html#autotoc_md7", null ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"index.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
