b96392c9b3cb74fe2d9f3768ed96b870c44b3092
Lucas Tesnier <lucas.tesnier@epitech.eu>
2022-10-10 15:38:46 +0200

    Add ArgumentHandler class inside Server architecture #81

---
+++ Server/ArgumentHandler.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2022
+** Project
+** File description:
+** ArgumentHandler
+*/
+
+#include "ArgumentHandler.hpp"
+
+using namespace server_data;
+
+ArgumentHandler::ArgumentHandler()
+{
+}
---
+++ Server/ArgumentHandler.hpp
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2022
+** Project
+** File description:
+** ArgumentHandler
+*/
+
+/// @file Server/ArgumentHandler.hpp
+
+#ifndef ARGUMENTHANDLER_HPP_
+#define ARGUMENTHANDLER_HPP_
+
+namespace server_data
+{
+    /// @brief Class holding parse of arguments from main. Possibility of extract the data to create some other class
+    class ArgumentHandler {
+      public:
+
+        /// @brief Construct a new ArgumentHandler object with default value
+        ArgumentHandler();
+
+        /// @brief Destroy the Argument Handler object
+        ~ArgumentHandler() = default;
+
+      protected:
+      private:
+    };
+} // namespace server_data
+
+#endif /* !ARGUMENTHANDLER_HPP_ */
