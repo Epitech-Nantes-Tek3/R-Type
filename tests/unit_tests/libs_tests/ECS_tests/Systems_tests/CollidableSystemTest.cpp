08845ea7cb8fd44402989b0e967ebb698641c759
Nathan <nathan.rousseau@epitech.eu>
2022-10-11 23:57:25 +0200

    Collidable System #80
    No collide test

--- tests/unit_tests/libs_tests/ECS_tests/Systems_tests/CollidableSystemTest.cpp
+++ tests/unit_tests/libs_tests/ECS_tests/Systems_tests/CollidableSystemTest.cpp
@@ -21,7 +21,18 @@
 
 using namespace ecs;
 
-Test(Toto, tata)
+Test(Collidable_Test, entities_does_not_collide)
 {
-    cr_assert_eq(42, 42);
+    World world(1);
+
+    std::size_t FirstId = world.addEntity().addComponent<Position>(10, 10).addComponent<Size>(15, 15).addComponent<Damage>(10).addComponent<Collidable>().addComponent<Enemy>().addComponent<Life>(20).getId();
+    std::size_t SecondId = world.addEntity().addComponent<Position>(1000, 1000).addComponent<Size>(15, 15).addComponent<Damage>(10).addComponent<Collidable>().addComponent<Player>().addComponent<Life>(20).getId();
+
+    world.addSystem<Collide>();
+
+    Life fst = world.getEntity(FirstId).getComponent<Life>();
+    Life snd = world.getEntity(SecondId).getComponent<Life>();
+
+    cr_assert_eq(20, fst.lifePoint);
+    cr_assert_eq(20, snd.lifePoint);
 }
