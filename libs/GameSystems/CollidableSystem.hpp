43bd3e3317dee5dce75d6adc1e3f649e6b489eba
Nathan <nathan.rousseau@epitech.eu>
2022-10-11 22:01:06 +0200

    Collidable system
    Add optimisations functions, still missing the logic part #84

--- libs/GameSystems/CollidableSystem.hpp
+++ libs/GameSystems/CollidableSystem.hpp
@@ -12,20 +12,68 @@
 
 namespace ecs
 {
-    /// @brief This is the Collidable System.
+    /// @brief This is the Collide System.
     /// This system will check every entities, see if they collide and apply damage on consequence
     /// Needed Component : Collidable, Position, Size, Damage
-    struct CollidableSystem : public System {
+    struct Collide : public System {
+        bool isSameWidth(Position &fstPos, Position &sndPos, Size &fstSize, Size &sndSize)
+        {
+            bool fstBelowSnd = fstPos.y <= sndPos.y && sndPos.y <= fstPos.y + fstSize.y;
+            bool fstAboveSnd = fstPos.y <= sndPos.y + sndSize.y && sndPos.y + sndSize.y <= fstPos.y + fstSize.y;
+            bool sndBelowFst = sndPos.y <= fstPos.y && fstPos.y <= sndPos.y + sndSize.y;
+            bool snfAboveFst = sndPos.y <= fstPos.y + fstSize.y && fstPos.y + fstSize.y <= sndPos.y + sndSize.y;
+
+            return fstBelowSnd || fstAboveSnd || sndBelowFst || snfAboveFst;
+        }
+
+        bool isSameHeight(Position &fstPos, Position &sndPos, Size &fstSize, Size &sndSize)
+        {
+            bool fstToTheRightSnd = fstPos.x <= sndPos.x && sndPos.x <= fstPos.x + fstSize.x;
+            bool fstToTheLeftSnd = fstPos.x <= sndPos.x + sndSize.x && sndPos.x + sndSize.x <= fstPos.x + fstSize.x;
+            bool sndToTheRightFst = sndPos.x <= fstPos.x && fstPos.x <= sndPos.x + sndSize.x;
+            bool sndToTheLeftFst = sndPos.x <= fstPos.x + fstSize.x && fstPos.x + fstSize.x <= sndPos.x + sndSize.x;
+
+            return fstToTheRightSnd || fstToTheLeftSnd || sndToTheRightFst || sndToTheLeftFst;
+        }
+
+        void collide(
+            std::vector<std::shared_ptr<ecs::Entity>> &first, std::vector<std::shared_ptr<ecs::Entity>> &second)
+        {
+
+        }
+
+        void enemyCollide(gtd::vector<std::shared_ptr<ecs::Entity>> &enemyProjectiles,
+            std::vector<std::shared_ptr<ecs::Entity>> &obstacles)
+        {
+            collide(enemyProjectiles, obstacles);
+        }
+
+        void allyCollide(std::vector<std::shared_ptr<ecs::Entity>> &allyEntities,
+            std::vector<std::shared_ptr<ecs::Entity>> &enemies,
+            std::vector<std::shared_ptr<ecs::Entity>> &enemyProjectiles,
+            std::vector<std::shared_ptr<ecs::Entity>> &obstacles)
+        {
+            collide(allyEntities, enemies);
+            collide(allyEntities, enemyProjectiles);
+            collide(allyEntities, obstacles);
+        }
+
         inline void run(World &world) override final
         {
-            std::vector<std::shared_ptr<ecs::Entity>> joined = world.joinEntities<Position, Size, Collidable, Damageb>();
-            unsigned int length = joined.size();
+            std::vector<std::shared_ptr<ecs::Entity>> players =
+                world.joinEntities<Position, Size, Collidable, Damage, Player>();
+            std::vector<std::shared_ptr<ecs::Entity>> alliedProjectiles =
+                world.joinEntities<Position, Size, Collidable, Damage, alliedProjectiles>();
+            std::vector<std::shared_ptr<ecs::Entity>> enemies =
+                world.joinEntities<Position, Size, Collidable, Damage, Enemy>();
+            std::vector<std::shared_ptr<ecs::Entity>> enemyProjectiles =
+                world.joinEntities<Position, Size, Collidable, Damage, EnemyProjectile>();
+            std::vector<std::shared_ptr<ecs::Entity>> obstacles =
+                world.joinEntities<Position, Size, Collidable, Damage, Obstacle>();
 
-            auto collide = [](std::shared_ptr<ecs::Entity> entityPtr) {
-
-            };
-            for (unsigned x = 0; x < length; ++x)
-              std::for_each(joined.begin() + x, joined.end(), collide);
+            allyCollide(players, enemies, enemyProjectiles, obstacles);
+            allyCollide(alliedProjectiles, enemies, enemyProjectiles, obstacles);
+            enemyCollide(enemyProjectiles, obstacles);
         }
     };
 } // namespace ecs
