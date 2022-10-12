cdc92f2991a930ce77ada888cef36c9bcf1ca80b
Bunny <tomduvalmahe44@gmail.com>
2022-10-12 19:25:00 +0200

    Add Documentation for create random enemy #84

--- libs/GameEntityManipulation/CreateEntitiesFunctions/CreateEnemyRandomPosition.hpp
+++ libs/GameEntityManipulation/CreateEntitiesFunctions/CreateEnemyRandomPosition.hpp
@@ -23,9 +23,19 @@
 
 namespace ecs
 {
-    std::size_t createNewEnemyRandom(World &world, const double multiplierAbscissa, const double multiplierOrdinate,
-        const short weight, const int size_x, const int size_y, const unsigned short life, const unsigned short damage,
-        const unsigned short damageRadius)
+    
+    /// @brief This function create a new Enemy Entity with random position and with these parameters
+    /// @param world The world in which the Enemy must be created
+     /// @param multiplierAbscissa The Velocity multiplierAbscissa for the new Enemy
+    /// @param multiplierOrdinate The Velocity multiplierOrdinate for the new Enemy
+    /// @param weight Weight of the Enemy
+    /// @param size_x Size x of the Enemy
+    /// @param size_y Size y of the Enemy
+    /// @param life Life of the Enemy
+    /// @param damage Damage of projectiles fired by this Enemy
+    /// @param damageRadius DamageRadius of projectiles fired by this Enemy
+    /// @return Id of the new Enemy in std::size_t
+    std::size_t createNewEnemyRandom(World &world, const double multiplierAbscissa, const double multiplierOrdinate, const short weight, const int size_x, const int size_y, const unsigned short life, const unsigned short damage, const unsigned short damageRadius)
     {
         return world.addEntity()
             .addComponent<Position>(world.getResource<RandomDevice>().randInt(1400, 1920),
