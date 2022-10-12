576209ddbc20183f09a30d4693148baa32d4002e
Bunny <tomduvalmahe44@gmail.com>
2022-10-12 18:58:39 +0200

    Create Enemy Random tested

--- libs/GameEntityManipulation/CreateEntitiesFunctions/CreateEnemyRandomPosition.hpp
+++ libs/GameEntityManipulation/CreateEntitiesFunctions/CreateEnemyRandomPosition.hpp
@@ -8,5 +8,39 @@
 #ifndef CREATEENEMYRANDOMPOSITION_HPP_
 #define CREATEENEMYRANDOMPOSITION_HPP_
 
+#include "GameComponents/CollidableComponent.hpp"
+#include "GameComponents/DamageComponent.hpp"
+#include "GameComponents/DamageRadiusComponent.hpp"
+#include "GameComponents/EnemyComponent.hpp"
+#include "GameComponents/LifeComponent.hpp"
+#include "GameComponents/LifeTimeComponent.hpp"
+#include "GameComponents/PositionComponent.hpp"
+#include "GameComponents/SizeComponent.hpp"
+#include "GameComponents/VelocityComponent.hpp"
+#include "GameComponents/WeightComponent.hpp"
+#include "GameSharedResources/Random.hpp"
+#include "World/World.hpp"
 
+namespace ecs
+{
+    std::size_t createNewEnemyRandom(World &world, const double multiplierAbscissa, const double multiplierOrdinate,
+        const short weight, const int size_x, const int size_y, const unsigned short life, const unsigned short damage,
+        const unsigned short damageRadius)
+    {
+        return world.addEntity()
+            .addComponent<Position>(world.getResource<RandomDevice>().randInt(1400, 1920),
+                world.getResource<RandomDevice>().randInt(0, 1080))
+            .addComponent<Weight>(weight)
+            .addComponent<Size>(size_x, size_y)
+            .addComponent<Lifetime>()
+            .addComponent<Life>(life)
+            .addComponent<Damage>(damage)
+            .addComponent<DamageRadius>(damageRadius)
+            .addComponent<Collidable>()
+            .addComponent<Velocity>(multiplierAbscissa, multiplierOrdinate)
+            .addComponent<Enemy>()
+            .getId();
+    }
+
+} // namespace ecs
 #endif /* !CREATEENEMYRANDOMPOSITION_HPP_ */
---
+++ tests/unit_tests/libs_tests/GameEntityManipulation_tests/CreateEntitiesFunctions_tests/CreateEnemyRandom_test.cpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2022
+** R-Type
+** File description:
+** CreateEnemyRandom_test
+*/
+
+#include <criterion/criterion.h>
+#include "World/World.hpp"
+#include "CreateEnemyRandomPosition.hpp"
+
+using namespace ecs;
+
+Test(CreateEnemyRandom_test, CreateEnemy)
+{
+    World world(1);
+
+    world.addResource<RandomDevice>();
+
+    std::size_t id_new_entity = createNewEnemyRandom(world, 1, 1, 1, 5, 5, 100, 10, 5);
+
+    cr_assert_eq(1, id_new_entity);
+}
