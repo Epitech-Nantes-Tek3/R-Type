698be36c8bc4e58baa306e0119a84c3450ca619c
Nathan <nathan.rousseau@epitech.eu>
2022-10-12 16:47:20 +0200

    GameClock Testing Done #77
    Only Missing System implementation

--- tests/unit_tests/libs_tests/ECS_tests/Resource_tests/GameClock_resource_tests.cpp
+++ tests/unit_tests/libs_tests/ECS_tests/Resource_tests/GameClock_resource_tests.cpp
@@ -7,8 +7,27 @@
 
 #include <criterion/criterion.h>
 #include "GameSharedResources/GameClock.hpp"
+#include "World/World.hpp"
+#include <unistd.h>
 
-Test(GameClock, basic_test)
+Test(GameClock, reset_clock_test)
 {
-    cr_assert_eq(1, 1);
+    World world(1);
+
+    world.addResource<GameClock>();
+    GameClock &clock = world.getResource<GameClock>();
+    sleep(5);
+    clock.resetClock();
+    cr_assert_eq(clock.elapsedTime() < 1.0, true);
+}
+
+Test(GameClock, elapsedTime_test)
+{
+    World world(1);
+
+    world.addResource<GameClock>();
+    GameClock &clock = world.getResource<GameClock>();
+    sleep(7);
+    cr_assert_eq(clock.elapsedTime() < 8.0, true);
+    cr_assert_eq(6.0 < clock.elapsedTime(), true);
 }
