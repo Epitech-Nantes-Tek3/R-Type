f2cf764439dc72717cf6a2c81243ca616bfb245a
Nathan <nathan.rousseau@epitech.eu>
2022-10-12 16:31:46 +0200

    GameClock Test
    Files created, Compilation done #77

---
+++ tests/unit_tests/libs_tests/ECS_tests/Resource_tests/GameClock_resource_tests.cpp
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2022
+** Project
+** File description:
+** GameClock_resource_tests
+*/
+
+#include <criterion/criterion.h>
+#include "GameSharedResources/GameClock.hpp"
+
+Test(GameClock, basic_test)
+{
+    cr_assert_eq(1, 1);
+}
