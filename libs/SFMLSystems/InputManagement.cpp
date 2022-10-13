5a666001677b4106ccd2926812106f9d062773d1
osvegn <thomas.prudhomme@epitech.eu>
2022-10-13 15:11:38 +0200

    Fix clang format in InputManagement #95

--- libs/SFMLSystems/InputManagement.cpp
+++ libs/SFMLSystems/InputManagement.cpp
@@ -6,49 +6,64 @@
 */
 
 #include "InputManagement.hpp"
-#include "World/World.hpp"
-#include "RenderWindowResource.hpp"
-#include "MouseInputComponent.hpp"
-#include "KeyboardInputComponent.hpp"
-#include "ControllerJoystickInputComponent.hpp"
-#include "ControllerButtonInputComponent.hpp"
 #include "ActionQueueComponent.hpp"
 #include "AllowControllerComponent.hpp"
 #include "AllowMouseAndKeyboardComponent.hpp"
+#include "ControllerButtonInputComponent.hpp"
+#include "ControllerJoystickInputComponent.hpp"
+#include "KeyboardInputComponent.hpp"
+#include "MouseInputComponent.hpp"
+#include "RenderWindowResource.hpp"
+#include "World/World.hpp"
 
 using namespace ecs;
 
 void InputManagement::run(World &world)
 {
     sf::Event event;
-    std::vector<std::shared_ptr<Entity>> Inputs = world.joinEntities<MouseInputComponent, KeyboardInputComponent, ControllerButtonInputComponent, ControllerJoystickInputComponent, ActionQueueComponent>();
+    std::vector<std::shared_ptr<Entity>> Inputs = world.joinEntities<MouseInputComponent, KeyboardInputComponent,
+        ControllerButtonInputComponent, ControllerJoystickInputComponent, ActionQueueComponent>();
 
     while (world.getResource<RenderWindowResource>().window.pollEvent(event)) {
         if (event.type == sf::Event::KeyPressed) {
             auto keyPressed = [event](std::shared_ptr<ecs::Entity> entityPtr) {
-                if (entityPtr->getComponent<KeyboardInputComponent>().keyboardMapActions.contains(event.key.code) && entityPtr->contains<AllowMouseAndKeyboardComponent>())
-                    entityPtr->getComponent<ActionQueueComponent>().actions.push(entityPtr->getComponent<KeyboardInputComponent>().keyboardMapActions[event.key.code].first);
+                if (entityPtr->getComponent<KeyboardInputComponent>().keyboardMapActions.contains(event.key.code)
+                    && entityPtr->contains<AllowMouseAndKeyboardComponent>())
+                    entityPtr->getComponent<ActionQueueComponent>().actions.push(
+                        entityPtr->getComponent<KeyboardInputComponent>().keyboardMapActions[event.key.code].first);
             };
             std::for_each(Inputs.begin(), Inputs.end(), keyPressed);
         }
         if (event.type == sf::Event::MouseButtonPressed) {
             auto mouseButtonPressed = [event](std::shared_ptr<ecs::Entity> entityPtr) {
-                if (entityPtr->getComponent<MouseInputComponent>().MouseMapActions.contains(event.mouseButton.button) && entityPtr->contains<AllowMouseAndKeyboardComponent>())
-                    entityPtr->getComponent<ActionQueueComponent>().actions.push(entityPtr->getComponent<MouseInputComponent>().MouseMapActions[event.mouseButton.button].first);
+                if (entityPtr->getComponent<MouseInputComponent>().MouseMapActions.contains(event.mouseButton.button)
+                    && entityPtr->contains<AllowMouseAndKeyboardComponent>())
+                    entityPtr->getComponent<ActionQueueComponent>().actions.push(
+                        entityPtr->getComponent<MouseInputComponent>().MouseMapActions[event.mouseButton.button].first);
             };
             std::for_each(Inputs.begin(), Inputs.end(), mouseButtonPressed);
         }
         if (event.type == sf::Event::JoystickButtonPressed) {
             auto joyButtonPressed = [event](std::shared_ptr<ecs::Entity> entityPtr) {
-                if (entityPtr->getComponent<ControllerButtonInputComponent>().controllerButtonMapActions.contains(event.joystickButton.button) && entityPtr->contains<AllowControllerComponent>())
-                    entityPtr->getComponent<ActionQueueComponent>().actions.push(entityPtr->getComponent<ControllerButtonInputComponent>().controllerButtonMapActions[event.joystickButton.button].first);
+                if (entityPtr->getComponent<ControllerButtonInputComponent>().controllerButtonMapActions.contains(
+                        event.joystickButton.button)
+                    && entityPtr->contains<AllowControllerComponent>())
+                    entityPtr->getComponent<ActionQueueComponent>().actions.push(
+                        entityPtr->getComponent<ControllerButtonInputComponent>()
+                            .controllerButtonMapActions[event.joystickButton.button]
+                            .first);
             };
             std::for_each(Inputs.begin(), Inputs.end(), joyButtonPressed);
         }
         if (event.type == sf::Event::JoystickMoved) {
             auto joyMovePressed = [event](std::shared_ptr<ecs::Entity> entityPtr) {
-                if (entityPtr->getComponent<ControllerJoystickInputComponent>().controllerJoystickMapActions.contains(event.joystickMove.axis) && entityPtr->contains<AllowControllerComponent>())
-                    entityPtr->getComponent<ActionQueueComponent>().actions.push(entityPtr->getComponent<ControllerJoystickInputComponent>().controllerJoystickMapActions[event.joystickMove.axis].first);
+                if (entityPtr->getComponent<ControllerJoystickInputComponent>().controllerJoystickMapActions.contains(
+                        event.joystickMove.axis)
+                    && entityPtr->contains<AllowControllerComponent>())
+                    entityPtr->getComponent<ActionQueueComponent>().actions.push(
+                        entityPtr->getComponent<ControllerJoystickInputComponent>()
+                            .controllerJoystickMapActions[event.joystickMove.axis]
+                            .first);
             };
             std::for_each(Inputs.begin(), Inputs.end(), joyMovePressed);
         }
