eb0b6cc862a440e1207de13a076c2818b36f6e24
osvegn <thomas.prudhomme@epitech.eu>
2022-10-14 01:54:22 +0200

    Add missing documentation in DrawComponent compare layer static function #129

--- libs/SFMLSystems/DrawComponents.hpp
+++ libs/SFMLSystems/DrawComponents.hpp
@@ -15,6 +15,10 @@ namespace ecs
 {
     /// @brief This class draw GraphicsComponents on window.
     struct DrawComponents : public System {
+        /// @brief Compare layer value to be sorted in run function.
+        /// @param e1 Value 1 to be compared.
+        /// @param e2 Value 2 to be compared.
+        /// @return return true if value 1 is greater than value 2, false otherwise
         static bool compareLayer(std::shared_ptr<Entity> e1, std::shared_ptr<Entity> e2);
         /// @brief The run function of this system. It will be used by the world in the function runSystems.
         /// @param world The corresponding world on which run this system.
