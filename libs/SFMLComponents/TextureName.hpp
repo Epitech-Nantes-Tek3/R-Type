6417aa9e88dd92cea4df7e31f6fc65be775bf12b
osvegn <thomas.prudhomme@epitech.eu>
2022-10-13 18:28:53 +0200

    INclude GraphicsTextureResource in TextureName component #127

--- libs/SFMLComponents/TextureName.hpp
+++ libs/SFMLComponents/TextureName.hpp
@@ -17,12 +17,12 @@ namespace ecs
     class TextureName : public Component {
         public:
             /// @brief The texture name stores to find the corresponding textureName.
-            textureName_e textureName;
+            GraphicsTextureResource::textureName_e textureName;
 
             /// @brief Constructor of the TextureName component
             /// @param newTextureName The value to set in the textureName, corresponding
             /// on the sf::Texture load in shared resource GraphicsTexture.
-            TextureName(textureName_e newTextureName = UNDEFINED);
+            TextureName(GraphicsTextureResource::textureName_e newTextureName = GraphicsTextureResource::UNDEFINED);
 
             /// @brief Default destructor of TextureName component.
             ~TextureName() = default;
