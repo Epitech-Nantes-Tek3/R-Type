c2d65aae5f3b162ed597e611d95a10ee0b3cb278
osvegn <thomas.prudhomme@epitech.eu>
2022-10-13 10:17:03 +0200

    Create DrawComponents::run function system to draw GraphicsRectangleComponent and GraphicsTextComponent. #95

--- libs/SFMLSystems/DrawComponents.cpp
+++ libs/SFMLSystems/DrawComponents.cpp
@@ -18,5 +18,15 @@ void DrawComponents::run(World &world)
 {
     std::vector<std::shared_ptr<Entity>> Inputs = world.joinEntities<LayerLvL>();
 
-    std::for_each(Inputs.begin(), Inputs.end(), layer);
+    for (std::size_t layerI = 1; layerI < LayerLvL::LAYER_NUMBER; layerI++) {
+        auto layer = [layerI, world](std::shared_ptr<Entity> entityPtr) {
+            if (entityPtr->getComponent<LayerLvL>().layer == layerI) {
+                if (entityPtr->contains<GraphicsRectangleComponent>())
+                    world.getResource<RenderWindowResource>().window.draw(entityPtr->getComponent<GraphicsRectangleComponent>().shape);
+                if (entityPtr->contains<GraphicsTextComponent>())
+                world.getResource<RenderWindowResource>().window.draw(entityPtr->getComponent<GraphicsTextComponent>().text);
+            }
+        };
+        std::for_each(Inputs.begin(), Inputs.end(), layer);
+    }
 }
