7d77fc98d9889df489dc63224e602759aa42a5e8
osvegn <thomas.prudhomme@epitech.eu>
2022-10-13 10:18:47 +0200

    Clear and display the window in the world. #95

--- libs/SFMLSystems/DrawComponents.cpp
+++ libs/SFMLSystems/DrawComponents.cpp
@@ -18,6 +18,7 @@ void DrawComponents::run(World &world)
 {
     std::vector<std::shared_ptr<Entity>> Inputs = world.joinEntities<LayerLvL>();
 
+    world.getResource<RenderWindowResource>().window.clear();
     for (std::size_t layerI = 1; layerI < LayerLvL::LAYER_NUMBER; layerI++) {
         auto layer = [layerI, world](std::shared_ptr<Entity> entityPtr) {
             if (entityPtr->getComponent<LayerLvL>().layer == layerI) {
@@ -29,4 +30,5 @@ void DrawComponents::run(World &world)
         };
         std::for_each(Inputs.begin(), Inputs.end(), layer);
     }
+    world.getResource<RenderWindowResource>().window.display();
 }
