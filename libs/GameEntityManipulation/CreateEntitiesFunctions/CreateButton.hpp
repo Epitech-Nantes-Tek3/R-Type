9ac71c3a2d6963f538ea7cf07ce55a4f3ba0708f
Bunny <tomduvalmahe44@gmail.com>
2022-10-14 02:56:14 +0200

    Add create GameEntity Button #84

---
+++ libs/GameEntityManipulation/CreateEntitiesFunctions/CreateButton.hpp
@@ -0,0 +1,47 @@
+/*
+** EPITECH PROJECT, 2022
+** R-Type
+** File description:
+** CreateButton
+*/
+
+#ifndef CREATEBUTTON_HPP_
+#define CREATEBUTTON_HPP_
+
+#include "GameComponents/CollidableComponent.hpp"
+#include "GameComponents/DamageComponent.hpp"
+#include "GameComponents/ButtonComponent.hpp"
+#include "GameComponents/PositionComponent.hpp"
+#include "GameComponents/SizeComponent.hpp"
+#include "GameComponents/VelocityComponent.hpp"
+#include "GameComponents/WeightComponent.hpp"
+#include "World/World.hpp"
+
+namespace ecs
+{
+    /// @brief This function creates a new Button Entity with these parameters
+    /// @param world The world in which the Button must be created
+    /// @param pos_x Position x of the Button
+    /// @param pos_y Position y of the Button
+    /// @param weight Weight of the Button
+    /// @param size_x Size x of the Button
+    /// @param size_y Size y of the Button
+    /// @param multiplierAbscissa The Velocity multiplierAbscissa for the new Button
+    /// @param multiplierOrdinate The Velocity multiplierOrdinate for the new Button
+    /// @return Id of the new Button in std::size_t
+    inline std::size_t createNewButton(World &world, const int pos_x, const int pos_y, const double multiplierAbscissa,
+        const double multiplierOrdinate, const short weight, const int size_x, const int size_y)
+    {
+        return world.addEntity()
+            .addComponent<Position>(pos_x, pos_y)
+            .addComponent<Weight>(weight)
+            .addComponent<Size>(size_x, size_y)
+            .addComponent<Collidable>()
+            .addComponent<Velocity>(multiplierAbscissa, multiplierOrdinate)
+            .addComponent<Button>()
+            .getId();
+    }
+
+} // namespace ecs
+
+#endif /* !CREATEBUTTON_HPP_ */
