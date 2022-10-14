851a4de441309ef9ed1568c2a6864704609d9d8a
Nathan <nathan.rousseau@epitech.eu>
2022-10-14 23:56:47 +0200

    EnemyShoot System : begin
    File Created and Compiled #156

---
+++ libs/GameSystems/EnemyShootSystem.cpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2022
+** R-Type
+** File description:
+** EnemyShootSystem
+*/
+
+#include "EnemyShootSystem.hpp"
+#include "GameComponents/EnemyComponent.hpp"
+#include "GameComponents/ShootingFrequencyComponent.hpp"
+#include "GameEntityManipulation/CreateEntitiesFunctions/CreateEnemyProjectile.hpp"
+
+using namespace ecs;
+
+void EnemyShootSystem::run(World &world)
+{
+    (void)world;
+}
