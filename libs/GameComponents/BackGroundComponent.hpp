f175c688201975754ada444644ccbd8097664619
Bunny <tomduvalmahe44@gmail.com>
2022-10-14 02:55:25 +0200

    Create library functions which create allEntity#84
    Add Background Component

---
+++ libs/GameComponents/BackGroundComponent.hpp
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2022
+** R-Type
+** File description:
+** BackGroundComponent
+*/
+
+#ifndef BACKGROUNDCOMPONENT_HPP_
+#define BACKGROUNDCOMPONENT_HPP_
+
+#include "Component/Component.hpp"
+
+namespace ecs
+{
+    /// @brief BackGround Component allows an entity to be used as a BackGround
+    class BackGround : public Component {};
+} // namespace ecs
+
+#endif /* !BACKGROUNDCOMPONENT_HPP_ */
