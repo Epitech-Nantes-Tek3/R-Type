875480eee3590369a179969db4eae24316353e39
Nathan <nathan.rousseau@epitech.eu>
2022-10-14 20:00:36 +0200

    Go Random System : intermediate
    Run function implemented, missing logic part #153

--- libs/GameSystems/EnemiesGoRandom.cpp
+++ libs/GameSystems/EnemiesGoRandom.cpp
@@ -6,11 +6,21 @@
 */
 
 #include "EnemiesGoRandom.hpp"
+#include "GameComponents/EnemyComponent.hpp"
+#include "GameComponents/PositionComponent.hpp"
+#include "GameComponents/VelocityComponent.hpp"
+#include "GameComponents/DestinationComponent.hpp"
 #include "GameSharedResources/Random.hpp"
 
 using namespace ecs;
 
 void EnemiesGoRandom::run(World &world)
 {
-    (void)world;
+    std::vector<std::shared_ptr<ecs::Entity>> joined = world.joinEntities<Enemy, Position, Velocity, Destination>();
+    RandomDevice &random = world.getResource<RandomDevice>();
+
+    auto randomMove = [](std::shared_ptr<ecs::Entity> entityPtr) {
+        
+    };
+    std::for_each(joined.begin(), joined.end(), randomMove);
 }
