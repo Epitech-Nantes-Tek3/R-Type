91c813fd02d39658e662ec087beedf37a724cd8c
Nathan <nathan.rousseau@epitech.eu>
2022-10-14 19:49:57 +0200

    Enemies Go Random system : Begin #153
    Files created and compiled

---
+++ libs/GameSystems/EnemiesGoRandom.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2022
+** Project
+** File description:
+** EnemiesGoRandom
+*/
+
+#include "EnemiesGoRandom.hpp"
+#include "GameSharedResources/Random.hpp"
+
+using namespace ecs;
+
+void EnemiesGoRandom::run(World &world)
+{
+    (void)world;
+}
