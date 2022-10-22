2a1af2f81ceba814ed851c0499e0d850aa76baac
osvegn <thomas.prudhomme@epitech.eu>
2022-10-22 23:25:05 +0200

    Move create projectile entity in a .cpp file #196

--- libs/GameEntityManipulation/CreateEntitiesFunctions/CreateProjectile.hpp
+++ libs/GameEntityManipulation/CreateEntitiesFunctions/CreateProjectile.hpp
@@ -11,19 +11,19 @@
 #include "GameComponents/CollidableComponent.hpp"
 #include "GameComponents/DamageComponent.hpp"
 #include "GameComponents/DamageRadiusComponent.hpp"
+#include "GameComponents/LayerLvL.hpp"
 #include "GameComponents/LifeComponent.hpp"
 #include "GameComponents/LifeTimeComponent.hpp"
 #include "GameComponents/NewlyCreated.hpp"
+#include "GameComponents/PlayerComponent.hpp"
 #include "GameComponents/PositionComponent.hpp"
 #include "GameComponents/ProjectileComponent.hpp"
 #include "GameComponents/SizeComponent.hpp"
 #include "GameComponents/VelocityComponent.hpp"
 #include "GameComponents/WeightComponent.hpp"
-#include "Transisthor/TransisthorECSLogic/Both/Components/Networkable.hpp"
 #include "GameSharedResources/Random.hpp"
+#include "Transisthor/TransisthorECSLogic/Both/Components/Networkable.hpp"
 #include "World/World.hpp"
-#include "GameComponents/PlayerComponent.hpp"
-#include "GameComponents/LayerLvL.hpp"
 
 namespace ecs
 {
@@ -37,35 +37,8 @@ namespace ecs
     /// @param uuid The uuid of the entity. Can be empty.
     /// @param networkId The id of the Networkable Component. In the client instance, it MUST NOT be filled in.
     /// @return  Id of the new Projectile in std::size_t
-    inline std::size_t createNewProjectile(World &world, const int posX, const int posY,
-        const double multiplierAbscissa, const double multiplierOrdinate, const unsigned short damage,
-        const std::string uuid = "", unsigned short networkId = 0)
-    {
-        Entity &entity = world.addEntity()
-                             .addComponent<Position>(posX, posY)
-                             .addComponent<Weight>(1)
-                             .addComponent<Size>(2, 1)
-                             .addComponent<LifeTime>(4.0)
-                             .addComponent<Life>(10)
-                             .addComponent<Damage>(damage)
-                             .addComponent<DamageRadius>(5)
-                             .addComponent<Collidable>()
-                             .addComponent<Projectile>()
-                             .addComponent<Velocity>(multiplierAbscissa, multiplierOrdinate);
-
-        if (networkId) {
-            // Case : Creation in a server instance
-            entity.addComponent<NewlyCreated>(uuid, false);
-            entity.addComponent<Networkable>(networkId);
-        } else {
-            // Case : Creation in a Client instance
-            if (uuid != "") {
-                // Special case : the client created the entity and not the server
-                entity.addComponent<NewlyCreated>(uuid, true);
-            }
-            entity.addComponent<LayerLvL>(LayerLvL::layer_e::PROJECTILE);
-        }
-        return entity.getId();
-    }
+    std::size_t createNewProjectile(World &world, const int posX, const int posY, const double multiplierAbscissa,
+        const double multiplierOrdinate, const unsigned short damage, const std::string uuid = "",
+        unsigned short networkId = 0);
 } // namespace ecs
 #endif /* !CREATEPROJECTILE_HPP_ */
