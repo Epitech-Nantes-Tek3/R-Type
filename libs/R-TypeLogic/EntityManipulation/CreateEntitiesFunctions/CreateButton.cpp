66a4423a13fcf107be09a377c37ec44da4b35c33
Nathan <nathan.rousseau@epitech.eu>
2022-10-25 10:21:20 +0200

    Add A Exit Button #218
    Change Create Button Function

--- libs/R-TypeLogic/EntityManipulation/CreateEntitiesFunctions/CreateButton.cpp
+++ libs/R-TypeLogic/EntityManipulation/CreateEntitiesFunctions/CreateButton.cpp
@@ -9,15 +9,11 @@
 
 namespace ecs
 {
-    std::size_t createNewButton(World &world, const int posX, const int posY, const double multiplierAbscissa,
-        const double multiplierOrdinate, const short weight, const int sizeX, const int sizeY)
+    std::size_t createNewButton(World &world, const int posX, const int posY, const int sizeX, const int sizeY)
     {
         return world.addEntity()
             .addComponent<Position>(posX, posY)
-            .addComponent<Weight>(weight)
             .addComponent<Size>(sizeX, sizeY)
-            .addComponent<Collidable>()
-            .addComponent<Velocity>(multiplierAbscissa, multiplierOrdinate)
             .addComponent<Button>()
             .getId();
     }
--- libs/R-TypeLogic/EntityManipulation/CreateEntitiesFunctions/CreateButton.hpp
+++ libs/R-TypeLogic/EntityManipulation/CreateEntitiesFunctions/CreateButton.hpp
@@ -10,12 +10,8 @@
 
 #include "World/World.hpp"
 #include "R-TypeLogic/Global/Components/ButtonComponent.hpp"
-#include "R-TypeLogic/Global/Components/CollidableComponent.hpp"
-#include "R-TypeLogic/Global/Components/DamageComponent.hpp"
 #include "R-TypeLogic/Global/Components/PositionComponent.hpp"
 #include "R-TypeLogic/Global/Components/SizeComponent.hpp"
-#include "R-TypeLogic/Global/Components/VelocityComponent.hpp"
-#include "R-TypeLogic/Global/Components/WeightComponent.hpp"
 
 namespace ecs
 {
@@ -23,14 +19,10 @@ namespace ecs
     /// @param world The world in which the Button must be created
     /// @param posX Position x of the Button
     /// @param posY Position y of the Button
-    /// @param weight Weight of the Button
     /// @param sizeX Size x of the Button
     /// @param sizeY Size y of the Button
-    /// @param multiplierAbscissa The Velocity multiplierAbscissa for the new Button
-    /// @param multiplierOrdinate The Velocity multiplierOrdinate for the new Button
     /// @return Id of the new Button in std::size_t
-    std::size_t createNewButton(World &world, const int posX, const int posY, const double multiplierAbscissa,
-        const double multiplierOrdinate, const short weight, const int sizeX, const int sizeY);
+    std::size_t createNewButton(World &world, const int posX, const int posY, const int sizeX, const int sizeY);
 } // namespace ecs
 
 #endif /* !CREATEBUTTON_HPP_ */
