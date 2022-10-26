04ed905270dbbaafdc0880f86815e4651a4193b2
Nathan <nathan.rousseau@epitech.eu>
2022-10-26 11:07:36 +0200

    Add Exit Button #218
    Add a Map of Button Action

---
+++ libs/R-TypeLogic/Global/SharedResources/ButtonActionMap.hpp
@@ -0,0 +1,49 @@
+/*
+** EPITECH PROJECT, 2022
+** Project
+** File description:
+** ButtonActionMap
+*/
+
+#ifndef BUTTONACTIONMAP_HPP_
+#define BUTTONACTIONMAP_HPP_
+
+#include <functional>
+#include "Resource/Resource.hpp"
+#include "World/World.hpp"
+#include <unordered_map>
+
+namespace ecs
+{
+    /// @brief This resource class store a map of button action to be executed on click
+    class ButtonActionMap : public Resource {
+      public:
+        /// @brief Enumeration of all the available actions
+        enum buttonAction_e {
+          UNDEFINED,
+          EXIT
+        };
+
+        /// @brief Map's name which contains the actions
+        using ActionList = std::unordered_map <buttonAction_e, std::function<void(World &)>>;
+
+        /// @brief Constructor of the map, calling the addAction Method
+        /// @param action Enum of the Action
+        /// @param fctPtr The fonction to be executed
+        ButtonActionMap(const buttonAction_e &action, std::function<void(World &)> fctPtr);
+
+        /// @brief Add a action to the ActionList
+        /// @param action Enum of the Action
+        /// @param fctPtr The fonction to be executed
+        void addAction(const buttonAction_e newAction, std::function<void(World &)> fctPtr);
+
+        /// @brief Default Constructor of the class
+        ~ButtonActionMap() = default;
+
+      private:
+        /// @brief The map where the actions are stored
+        ActionList _actionList;
+    };
+} // namespace ecs
+
+#endif /* !BUTTONACTIONMAP_HPP_ */
