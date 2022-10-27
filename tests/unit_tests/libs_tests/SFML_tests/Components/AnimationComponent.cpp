c7d4c01c8911285c96a0c59c3d47bdf733d7622e
osvegn <thomas.prudhomme@epitech.eu>
2022-10-27 13:35:24 +0200

    Add animationComponent tests #223

---
+++ tests/unit_tests/libs_tests/SFML_tests/Components/AnimationComponent.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2022
+** R-Type [WSL : Ubuntu]
+** File description:
+** AnimationComponent
+*/
+
+#include <criterion/criterion.h>
+#include "AnimationComponent.hpp"
+
+Test(AnimationComponent, create_class)
+{
+    AnimationComponent a;
+
+    cr_assert_eq(1, 1);
+}
