7a2e283e557d80816b5b8c7aa37630f82b36d8ac
osvegn <thomas.prudhomme@epitech.eu>
2022-10-27 22:04:02 +0200

    Create AnimationSystem.hpp file #242

---
+++ libs/GraphicECS/SFML/Systems/AnimationSystem.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2022
+** rtype
+** File description:
+** AnimationSystem
+*/
+
+#ifndef ANIMATIONSYSTEM_HPP_
+#define ANIMATIONSYSTEM_HPP_
+
+class AnimationSystem {
+    public:
+        AnimationSystem();
+        ~AnimationSystem();
+
+    protected:
+    private:
+};
+
+#endif /* !ANIMATIONSYSTEM_HPP_ */
