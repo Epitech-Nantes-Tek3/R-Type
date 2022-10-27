ef9cb402175c6868d71521e9300ce4612d80edf2
Quentin C. | ZQUEMA <zquema1@gmail.com>
2022-10-27 17:53:00 +0200

    Add std::mutex inheritance to the Resource class #238
    The std::mutex inheritance is in order to allows to lock a Resource when something wants to use/modify it

--- libs/ECS/Resource/Resource.hpp
+++ libs/ECS/Resource/Resource.hpp
@@ -7,15 +7,20 @@
 
 #pragma once
 
+#include <mutex>
 
 namespace ecs
 {
     /// @brief This is the Resource Class for ECS
     /// The base Resource type, all Resources must inherit from Resource.
-    class Resource {
+    /// The std::mutex inheritance is in order to allows to lock a Resource when something wants to use/modify it
+    class Resource : public std::mutex {
       public:
         /// @brief This is the default destructor of Resource Class
         virtual ~Resource() = default;
+
+        ///@brief Delete the copy constructor because a mutex can't be copied
+        Resource(const Resource &) = delete;
 
       protected:
         /// @brief This is the constructor of Resource Class
