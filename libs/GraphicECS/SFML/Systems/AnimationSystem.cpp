518b355aeb437273bef42bc234d950672cf70674
osvegn <thomas.prudhomme@epitech.eu>
2022-10-27 23:02:09 +0200

    Update texture set on the shape #242

--- libs/GraphicECS/SFML/Systems/AnimationSystem.cpp
+++ libs/GraphicECS/SFML/Systems/AnimationSystem.cpp
@@ -9,6 +9,7 @@
 #include "GraphicsRectangleComponent.hpp"
 // #include "AnimationDelayComponent.hpp"
 #include "AnimationComponent.hpp"
+#include "GraphicsTextureResource.hpp"
 
 using namespace ecs;
 
@@ -16,8 +17,15 @@ void AnimationSystem::run(World &world)
 {
     std::vector<std::shared_ptr<Entity>> shapes = world.joinEntities<GraphicsRectangleComponent, AnimationComponent/*, AnimationDelayComponent*/>();
 
-    auto shape = [](std::shared_ptr<Entity> entity) {
-        entity->getComponent<GraphicsRectangleComponent>().shape;
+    auto shape = [&world](std::shared_ptr<Entity> entity) {
+        using texturesNamesVector = std::vector<ecs::GraphicsTextureResource::textureName_e>;
+        using texturesMap = std::unordered_map<ecs::GraphicsTextureResource::textureName_e, std::shared_ptr<sf::Texture>>;
+
+        texturesNamesVector texturesNames = entity->getComponent<AnimationComponent>().textures;
+        std::size_t currentTexture = entity->getComponent<AnimationComponent>().currentTexture;
+        texturesMap textures = world.getResource<GraphicsTextureResource>()._texturesList;
+
+        entity->getComponent<GraphicsRectangleComponent>().shape.setTexture(textures.at(currentTexture).get());
     };
     std::for_each(shapes.begin(), shapes.end(), shape);
 }
