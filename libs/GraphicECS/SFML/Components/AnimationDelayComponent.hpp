b50263c0c5484561fee7423820f254cb27036c11
osvegn <thomas.prudhomme@epitech.eu>
2022-10-27 14:30:10 +0200

    Create AnimationDelayComponent.hpp file #224

---
+++ libs/GraphicECS/SFML/Components/AnimationDelayComponent.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2022
+** R-Type [WSL : Ubuntu]
+** File description:
+** AnimationDelayComponent
+*/
+
+#ifndef ANIMATIONDELAYCOMPONENT_HPP_
+#define ANIMATIONDELAYCOMPONENT_HPP_
+
+class AnimationDelayComponent {
+    public:
+        AnimationDelayComponent();
+        ~AnimationDelayComponent();
+
+    protected:
+    private:
+};
+
+#endif /* !ANIMATIONDELAYCOMPONENT_HPP_ */
