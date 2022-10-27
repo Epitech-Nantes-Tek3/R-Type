8e6ce21976d453f9f6555782c0aa0a32aceacf06
Lucas Tesnier <lucas.tesnier@epitech.eu>
2022-10-27 14:43:06 +0200

    Implement a new frequency gestion in template format #227

--- libs/R-TypeLogic/Global/Systems/UpdateClockSystem.cpp
+++ libs/R-TypeLogic/Global/Systems/UpdateClockSystem.cpp
@@ -17,17 +17,7 @@ void UpdateClock::run(World &world)
     GameClock &clock = world.getResource<GameClock>();
     std::vector<std::shared_ptr<ecs::Entity>> joined = world.joinEntities<ShootingFrequency>();
 
-    auto updateShootingFrequency = [&world, &clock](std::shared_ptr<ecs::Entity> entityPtr) {
-        ShootingFrequency &freq = entityPtr.get()->getComponent<ShootingFrequency>();
-        double delta = freq.frequency.count() - clock.getElapsedTime();
-
-        if (delta <= 0.0) {
-            freq.frequency = duration<double>(0);
-            return;
-        }
-        freq.frequency = duration<double>(delta);
-    };
-
     clock.resetClock();
-    std::for_each(joined.begin(), joined.end(), updateShootingFrequency);
+    for (auto it : joined)
+        updateAFrequencyComponent<ShootingFrequency>(clock, it);
 }
