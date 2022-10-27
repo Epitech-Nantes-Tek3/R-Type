387ac93b0d4c69cc968dd3f710003c9d142c5c02
osvegn <thomas.prudhomme@epitech.eu>
2022-10-27 13:14:03 +0200

    Create AnimationComponent with a vector of textures and default destructor and default destructor #223

---
+++ libs/GraphicECS/SFML/Components/AnimationComponent.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2022
+** R-Type [WSL : Ubuntu]
+** File description:
+** AnimationComponent
+*/
+
+#include "AnimationComponent.hpp"
+
+AnimationComponent::AnimationComponent()
+{
+}
+
+AnimationComponent::~AnimationComponent()
+{
+}
---
+++ libs/GraphicECS/SFML/Components/AnimationComponent.hpp
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2022
+** R-Type [WSL : Ubuntu]
+** File description:
+** AnimationComponent
+*/
+
+#ifndef ANIMATIONCOMPONENT_HPP_
+#define ANIMATIONCOMPONENT_HPP_
+
+#include <vector>
+#include "Component/Component.hpp"
+#include "GraphicECS/SFML/Resources/GraphicsTextureResource.hpp"
+
+namespace ecs
+{
+    class AnimationComponent : public Component {
+        public:
+            /// @brief A vector of textureName to store textures used for the animation.
+            std::vector<GraphicsTextureResource::textureName_e> textures;
+
+            /// @brief Default constructor of AnimationComponent.
+            AnimationComponent() = default;
+
+            /// @brief Default destructor of AnimationComponent.
+            ~AnimationComponent() = default;
+    };
+}
+
+#endif /* !ANIMATIONCOMPONENT_HPP_ */
