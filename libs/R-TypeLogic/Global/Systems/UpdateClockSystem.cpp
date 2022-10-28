cf79464bd18df762ea67dbceecc72a72c3947fb1
osvegn <thomas.prudhomme@epitech.eu>
2022-10-29 00:54:45 +0200

    Update Animation delay in UpdateClockSystem #242

--- libs/R-TypeLogic/Global/Systems/UpdateClockSystem.cpp
+++ libs/R-TypeLogic/Global/Systems/UpdateClockSystem.cpp
@@ -8,8 +8,10 @@
 #include "UpdateClockSystem.hpp"
 #include <chrono>
 #include <mutex>
+#include "AnimationDelayComponent.hpp"
 #include "R-TypeLogic/Global/Components/ShootingFrequencyComponent.hpp"
 #include "R-TypeLogic/Server/Components/AfkFrequencyComponent.hpp"
+
 using namespace ecs;
 
 void UpdateClock::run(World &world)
@@ -18,10 +20,14 @@ void UpdateClock::run(World &world)
     auto guard = std::lock_guard(clock);
     std::vector<std::shared_ptr<ecs::Entity>> joinedShoot = world.joinEntities<ShootingFrequency>();
     std::vector<std::shared_ptr<ecs::Entity>> joinedAfk = world.joinEntities<AfkFrequency>();
+    std::vector<std::shared_ptr<ecs::Entity>> joinedAnimation =
+        world.joinEntities<graphicECS::SFML::Components::AnimationDelayComponent>();
 
     clock.resetClock();
     for (auto it : joinedShoot)
         updateAFrequencyComponent<ShootingFrequency>(clock, it);
     for (auto it : joinedAfk)
         updateAFrequencyComponent<AfkFrequency>(clock, it);
+    for (auto it : joinedAnimation)
+        updateAFrequencyComponent<graphicECS::SFML::Components::AnimationDelayComponent>(clock, it);
 }
