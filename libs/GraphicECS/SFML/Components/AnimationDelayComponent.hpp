2b979f1702b78e54e048e84259f72a0271517280
osvegn <thomas.prudhomme@epitech.eu>
2022-10-28 23:53:50 +0200

    Use std::chrono instead of double #224

--- libs/GraphicECS/SFML/Components/AnimationDelayComponent.hpp
+++ libs/GraphicECS/SFML/Components/AnimationDelayComponent.hpp
@@ -8,25 +8,27 @@
 #ifndef ANIMATIONDELAYCOMPONENT_HPP_
 #define ANIMATIONDELAYCOMPONENT_HPP_
 
+#include <chrono>
 #include "Component/Component.hpp"
 
 namespace ecs
 {
     /// @brief This component allows to know the delay between each frame of an animation.
     class AnimationDelayComponent : public Component {
-        public:
-            /// @brief The delay between each frame of an animation as milliseconds.
-            double delay;
+      public:
+        /// @brief The delay between each frame of an animation as milliseconds.
+        std::chrono::duration<double> delay;
 
-            /// @brief The default delay of the animation as milliseconds.
-            const double baseDelay;
+        /// @brief The default delay of the animation as milliseconds.
+        const std::chrono::duration<double> baseDelay;
 
-            /// @brief Construct the class to set default values.
-            /// @param d Delay to set the baseDelay of the animation. Default value is 500.0 milliseconds.
-            AnimationDelayComponent(const double &d = 500.0) : baseDelay(d), delay(d) {};
+        /// @brief Construct the class to set default values.
+        /// @param d Delay to set the baseDelay of the animation. Default value is 500.0 milliseconds.
+        AnimationDelayComponent(const double &d = 500.0)
+            : baseDelay(std::chrono::duration<double>(d)), delay(std::chrono::duration<double>(d)){};
 
-            /// @brief Default destructor.
-            ~AnimationDelayComponent() = default;
+        /// @brief Default destructor.
+        ~AnimationDelayComponent() = default;
     };
 } // namespace ecs
 
