610e61ae129cd8873144eea51e2b5c98c64dc46f
osvegn <thomas.prudhomme@epitech.eu>
2022-10-29 22:21:12 +0200

    Create first basic test for animationSystem #266

---
+++ tests/unit_tests/libs_tests/SFML_tests/Systems/AnimationSystem_tests.cpp
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2022
+** rtype
+** File description:
+** AnimationSystem_tests
+*/
+
+#include <criterion/criterion.h>
+#include "AnimationComponent.hpp"
+#include "AnimationFrequencyComponent.hpp"
+#include "AnimationSystem.hpp"
+#include "World/World.hpp"
+
+using namespace graphicECS::SFML::Components;
+using namespace graphicECS::SFML::Systems;
+
+Test(AnimationSystem, create_basic_animation)
+{
+    World world(1);
+
+    world.addEntity().addComponent<AnimationComponent>().addComponent<AnimationFrequencyComponent>();
+    world.addSystem<AnimationSystem>();
+
+    world.runSystems();
+    cr_assert_eq(1, 1);
+}
