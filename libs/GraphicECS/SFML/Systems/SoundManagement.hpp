d15378364f536a1657f36c5504a3250b8e059e09
Bunny <tomduvalmahe44@gmail.com>
2022-10-29 11:24:26 +0200

    Change the documentation for SoundManagement #205

--- libs/GraphicECS/SFML/Systems/SoundManagement.hpp
+++ libs/GraphicECS/SFML/Systems/SoundManagement.hpp
@@ -16,9 +16,16 @@ namespace graphicECS::SFML::Systems
 {
     struct SoundManagement : public System {
       public:
+
+        /// @brief The run function of this system. It will be used by the world in the function runSystems.
+        /// @param world The corresponding world on which run this system.
         void run(World &world);
 
       private:
+        /// @brief This function Stoped the sound if it's status component is Stoped
+        /// It also resets the playing position (unlike pause()).
+        /// @param world The corresponding world on which run this system.
+        /// @param sound The Entity Component of the song (to find it's name and status)
         inline void stopSound(World &world, SoundName &sound)
         {
             world.getResource<SoundResource>()._soundsList.at(sound.getName())->stop();
