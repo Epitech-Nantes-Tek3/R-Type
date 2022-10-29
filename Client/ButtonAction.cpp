9c21086785081a7edc518b30eb025be8cd8d6811
Nathan <nathan.rousseau@epitech.eu>
2022-10-29 12:10:17 +0200

    Exit Function for Button now properly handled
    Sending a SINGINT, thanks to @Waroth with its perfect handling

--- Client/ButtonAction.cpp
+++ Client/ButtonAction.cpp
@@ -13,9 +13,6 @@ using namespace graphicECS::SFML::Resources;
 
 void exitWindow(World &world)
 {
-    // TO CHANGE WHEN PROPER HANDLE OF SIGINT WILL BE IMPLEMENTED
-    // At this moment, we can't lock the resource here because we can't close the window
-    // This will be fixed when the above change will be implemented
-    RenderWindowResource &resource = world.getResource<RenderWindowResource>();
-    resource.window.close();
+    (void)world;
+    std::raise(SIGINT);
 }
