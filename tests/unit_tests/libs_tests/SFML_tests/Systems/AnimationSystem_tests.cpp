cfdedb495c8066cc7f369564aa5c87efcd626850
osvegn <thomas.prudhomme@epitech.eu>
2022-10-29 23:36:23 +0200

    Test updateAnimation function in AnimationSystem #266

--- tests/unit_tests/libs_tests/SFML_tests/Systems/AnimationSystem_tests.cpp
+++ tests/unit_tests/libs_tests/SFML_tests/Systems/AnimationSystem_tests.cpp
@@ -58,3 +58,45 @@ Test(AnimationSystem, create_basic_animation)
     world.runSystems();
     cr_assert_eq(1, 1);
 }
+
+Test(AnimationSystem, use_basic_animation_and_wait_one_second)
+{
+    World world(1);
+
+    world.addEntity()
+        .addComponent<AnimationComponent>()
+        .addComponent<AnimationFrequencyComponent>()
+        .addComponent<GraphicsRectangleComponent>();
+    world.addResource<GameClock>();
+    world.addResource<GraphicsTextureResource>(GraphicsTextureResource::PLAYER_STATIC_1,
+        "assets/EpiSprite/BasicPlayerSpriteSheet.gif", sf::Vector2f(534 / 16 * 8, 0), sf::Vector2f(534 / 16, 34));
+    world.getResource<GraphicsTextureResource>().addTexture(GraphicsTextureResource::PLAYER_STATIC_2,
+        "assets/EpiSprite/BasicPlayerSpriteSheet.gif", sf::Vector2f(534 / 16 * 9, 0), sf::Vector2f(534 / 16, 34));
+    world.getResource<GraphicsTextureResource>().addTexture(GraphicsTextureResource::PLAYER_STATIC_3,
+        "assets/EpiSprite/BasicPlayerSpriteSheet.gif", sf::Vector2f(534 / 16 * 10, 0), sf::Vector2f(534 / 16, 34));
+    world.getResource<GraphicsTextureResource>().addTexture(GraphicsTextureResource::PLAYER_STATIC_4,
+        "assets/EpiSprite/BasicPlayerSpriteSheet.gif", sf::Vector2f(534 / 16 * 11, 0), sf::Vector2f(534 / 16, 34));
+    world.getResource<GraphicsTextureResource>().addTexture(GraphicsTextureResource::PLAYER_STATIC_5,
+        "assets/EpiSprite/BasicPlayerSpriteSheet.gif", sf::Vector2f(534 / 16 * 12, 0), sf::Vector2f(534 / 16, 34));
+    world.getResource<GraphicsTextureResource>().addTexture(GraphicsTextureResource::PLAYER_STATIC_6,
+        "assets/EpiSprite/BasicPlayerSpriteSheet.gif", sf::Vector2f(534 / 16 * 13, 0), sf::Vector2f(534 / 16, 34));
+    world.getResource<GraphicsTextureResource>().addTexture(GraphicsTextureResource::PLAYER_STATIC_7,
+        "assets/EpiSprite/BasicPlayerSpriteSheet.gif", sf::Vector2f(534 / 16 * 14, 0), sf::Vector2f(534 / 16, 34));
+    world.getResource<GraphicsTextureResource>().addTexture(GraphicsTextureResource::PLAYER_STATIC_8,
+        "assets/EpiSprite/BasicPlayerSpriteSheet.gif", sf::Vector2f(534 / 16 * 15, 0), sf::Vector2f(534 / 16, 34));
+    auto animation = world.joinEntities<AnimationComponent>();
+    for (auto &it : animation) {
+        it->getComponent<AnimationComponent>().textures.push_back(GraphicsTextureResource::PLAYER_STATIC_1);
+        it->getComponent<AnimationComponent>().textures.push_back(GraphicsTextureResource::PLAYER_STATIC_2);
+        it->getComponent<AnimationComponent>().textures.push_back(GraphicsTextureResource::PLAYER_STATIC_3);
+        it->getComponent<AnimationComponent>().textures.push_back(GraphicsTextureResource::PLAYER_STATIC_4);
+        it->getComponent<AnimationComponent>().textures.push_back(GraphicsTextureResource::PLAYER_STATIC_5);
+        it->getComponent<AnimationComponent>().textures.push_back(GraphicsTextureResource::PLAYER_STATIC_6);
+        it->getComponent<AnimationComponent>().textures.push_back(GraphicsTextureResource::PLAYER_STATIC_7);
+        it->getComponent<AnimationComponent>().textures.push_back(GraphicsTextureResource::PLAYER_STATIC_8);
+    }
+    world.addSystem<AnimationSystem>();
+    sleep(1);
+    world.runSystems();
+    cr_assert_eq(1, 1);
+}
