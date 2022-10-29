5a9bb3e60acc1c704f23f4858bf60475e8371218
Bunny <tomduvalmahe44@gmail.com>
2022-10-29 13:00:52 +0200

    Add System class MusicManagement #206

--- libs/GraphicECS/SFML/Systems/MusicManagement.hpp
+++ libs/GraphicECS/SFML/Systems/MusicManagement.hpp
@@ -8,6 +8,13 @@
 #ifndef MUSICMANAGEMENT_HPP_
 #define MUSICMANAGEMENT_HPP_
 
+#include "System/System.hpp"
+#include "World/World.hpp"
+#include "GraphicECS/SFML/Components/MusicName.hpp"
 
-
+namespace graphicECS::SFML::Systems
+{
+    struct MusicManagement : public System {
+    };
+} // namespace graphicECS::SFML::Systems
 #endif /* !MUSICMANAGEMENT_HPP_ */
