3a2e1611a5e68e16f4d1ba973943d8c4bb92240f
Nathan <nathan.rousseau@epitech.eu>
2022-10-29 14:39:15 +0200

    Bigger Buttons for Options Menu #220

--- Client/ClientRoom.cpp
+++ Client/ClientRoom.cpp
@@ -300,7 +300,7 @@ void ClientRoom::_initButtons()
     ButtonActionMap &actionsList = _worldInstance->getResource<ButtonActionMap>();
     actionsList.addAction(ButtonActionMap::RESUME, std::function<void(World &)>(resumeGame));
     actionsList.addAction(ButtonActionMap::PAUSE, std::function<void(World &)>(pauseGame));
-    createNewButton(*(_worldInstance.get()), 0, 0, 34, 34, ButtonActionMap::PAUSE, LayerLvL::BUTTON, MenuStates::IN_GAME);
-    createNewButton(*(_worldInstance.get()), 500, 300, 34, 34, ButtonActionMap::RESUME, LayerLvL::BUTTON, MenuStates::GAME_PAUSED);
-    createNewButton(*(_worldInstance.get()), 500, 500, 34, 34, ButtonActionMap::EXIT, LayerLvL::BUTTON, MenuStates::GAME_PAUSED);
+    createNewButton(*(_worldInstance.get()), 0, 0, 68, 68, ButtonActionMap::PAUSE, LayerLvL::BUTTON, MenuStates::IN_GAME);
+    createNewButton(*(_worldInstance.get()), 909, 200, 102, 102, ButtonActionMap::RESUME, LayerLvL::BUTTON, MenuStates::GAME_PAUSED);
+    createNewButton(*(_worldInstance.get()), 909, 500, 102, 102, ButtonActionMap::EXIT, LayerLvL::BUTTON, MenuStates::GAME_PAUSED);
 }
