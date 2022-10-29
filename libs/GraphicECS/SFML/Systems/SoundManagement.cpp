d3faf6bd2cfa60d83508ea6c313c6a5de7e9a877
Bunny <tomduvalmahe44@gmail.com>
2022-10-29 11:30:42 +0200

    Change the namespace in SFML::Systems  #205

--- libs/GraphicECS/SFML/Systems/SoundManagement.cpp
+++ libs/GraphicECS/SFML/Systems/SoundManagement.cpp
@@ -7,23 +7,26 @@
 
 #include "SoundManagement.hpp"
 
-void SoundManagement::run(World &world)
+namespace graphicECS::SFML::Systems
 {
-    std::vector<std::shared_ptr<ecs::Entity>> joined = world.joinEntities<SoundName>();
+    void SoundManagement::run(World &world)
+    {
+        std::vector<std::shared_ptr<ecs::Entity>> joined = world.joinEntities<SoundName>();
 
-    auto stopSound = [&world](std::shared_ptr<ecs::Entity> entityPtr) {
-        SoundName sound = entityPtr.get()->getComponent<SoundName>();
+        auto stopSound = [&world](std::shared_ptr<ecs::Entity> entityPtr) {
+            SoundName sound = entityPtr.get()->getComponent<SoundName>();
 
-        if (sound.getStatus() == SoundName::STOPED) {
-            stopSound(world, sound);
-        }
-        if (sound.getStatus() == SoundName::PAUSED) {
-            pauseSound(world, sound);
-        }
-        if (sound.getStatus() == SoundName::PLAY) {
-            playSound(world, sound);
-        }
-    };
+            if (sound.getStatus() == SoundName::STOPED) {
+                stopSound(world, sound);
+            }
+            if (sound.getStatus() == SoundName::PAUSED) {
+                pauseSound(world, sound);
+            }
+            if (sound.getStatus() == SoundName::PLAY) {
+                playSound(world, sound);
+            }
+        };
 
-    std::for_each(joined.begin(), joined.end(), stopSound);
-}
+        std::for_each(joined.begin(), joined.end(), stopSound);
+    }
+} // namespace graphicECS::SFML::Systems
--- libs/GraphicECS/SFML/Systems/SoundManagement.hpp
+++ libs/GraphicECS/SFML/Systems/SoundManagement.hpp
@@ -8,21 +8,19 @@
 #ifndef SOUNDMANAGEMENT_HPP_
 #define SOUNDMANAGEMENT_HPP_
 
-#include "GraphicECS/SFML/Components/SoundName.hpp"
 #include "System/System.hpp"
 #include "World/World.hpp"
+#include "GraphicECS/SFML/Components/SoundName.hpp"
 
 namespace graphicECS::SFML::Systems
 {
     struct SoundManagement : public System {
       public:
-
         /// @brief The run function of this system. It will be used by the world in the function runSystems.
         /// @param world The corresponding world on which run this system.
         void run(World &world);
 
-      private:
-        /// @brief This function Stoped the sound if it's status component is Stoped
+        /// @brief This function Stoped the sound if it's status component is STOPED
         /// It also resets the playing position (unlike pause()).
         /// @param world The corresponding world on which run this system.
         /// @param sound The Entity Component of the song (to find it's name and status)
@@ -30,10 +28,18 @@ namespace graphicECS::SFML::Systems
         {
             world.getResource<SoundResource>()._soundsList.at(sound.getName())->stop();
         };
+
+        /// @brief This function Paused the sound if it's status component is PAUSED
+        /// @param world The corresponding world on which run this system.
+        /// @param sound The Entity Component of the song (to find it's name and status)
         inline void pauseSound(World &world, SoundName &sound)
         {
             world.getResource<SoundResource>()._soundsList.at(sound.getName())->pause();
         };
+
+        /// @brief This function Play the sound if it's status component is PLAY
+        /// @param world The corresponding world on which run this system.
+        /// @param sound The Entity Component of the song (to find it's name and status)
         inline void playSound(World &world, SoundName &sound)
         {
             world.getResource<SoundResource>()._soundsList.at(sound.getName())->play();
