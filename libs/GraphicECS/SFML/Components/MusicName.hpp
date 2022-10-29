ec126840f33ee300be948901e8b542bb733d626e
Bunny <tomduvalmahe44@gmail.com>
2022-10-29 11:44:41 +0200

    Add function to manage Status and Name #205
    Add get and set functions for Status and Name

--- libs/GraphicECS/SFML/Components/MusicName.hpp
+++ libs/GraphicECS/SFML/Components/MusicName.hpp
@@ -11,11 +11,12 @@
 #include "Component/Component.hpp"
 #include "GraphicECS/SFML/Resources/MusicResource.hpp"
 
-namespace ecs
+namespace graphicECS::SFML::Components
 {
     /// @brief MusicName stores the key as enum to the corresponding music stored in shared resource MusicResource.
     class MusicName : public Component {
       public:
+
         /// @brief Enumeration of all status available for a Music.
         /// Status STOPED specify that a music is STOP but at it's begin
         enum status_e { STOPED, PAUSED, PLAYING };
@@ -27,14 +28,25 @@ namespace ecs
         /// @param newMusicName The value to set in the musicName, corresponding
         /// on the sf::Music load in shared resource Music.
         MusicName(MusicResource::musicName_e newMusicName = MusicResource::UNDEFINED, status_e status = status_e::STOPED)
-            : musicName(newMusicName){};
+            : musicName(newMusicName), _status(status) {};
 
         /// @brief Default destructor of MusicName component.
         ~MusicName() = default;
 
+        /// @brief This function return the status of the Music
+        inline status_e getStatus() const { return _status; };
+
+        /// @brief This function set the status of the Music
+        inline void setStatus(status_e status) { _status = status; };
+
+        /// @brief This function return the name of the Music
+        inline MusicResource::musicName_e getName() const { return musicName; };
+
+        /// @brief This function set the name of the Music
+        inline void setName(MusicResource::musicName_e name) { musicName = name; };
       private:
         status_e _status;
     };
-} // namespace ecs
+} // namespace graphicECS::SFML::Components
 
 #endif /* !MUSICNAME_HPP_ */
