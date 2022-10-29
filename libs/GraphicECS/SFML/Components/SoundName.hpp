a65d9d20d2f4f4059154114ffd98498caa8cacfa
Bunny <tomduvalmahe44@gmail.com>
2022-10-29 11:41:28 +0200

    Add function to manage sound status and name #203
    Add getStatus and setStatus
    Add getName and setName

--- libs/GraphicECS/SFML/Components/SoundName.hpp
+++ libs/GraphicECS/SFML/Components/SoundName.hpp
@@ -13,22 +13,42 @@
 
 namespace ecs
 {
-    /// @brief SoundName stores the key as enum to the corresponding sound stored in shared resource SoundResource.
+
+    /// @brief SoundName stores the key as enum to the corresponding music stored in shared resource SoundResource.
     class SoundName : public Component {
       public:
-        /// @brief The sound name stores an enum to find which sound it needs in SoundResources shared resource.
+        /// @brief Enumeration of all status available for a Sound.
+        /// Status STOPED specify that a music is STOP but at it's begin
+        enum status_e { STOPED, PAUSED, PLAYING };
+
+        /// @brief The music name stores an enum to find which music it needs in SoundResources shared resource
         SoundResource::soundName_e soundName;
 
         /// @brief Constructor of the SoundName component
         /// @param newSoundName The value to set in the soundName, corresponding
         /// on the sf::Sound load in shared resource Sound.
-        SoundName(SoundResource::soundName_e newSoundName = SoundResource::UNDEFINED)
-            : soundName(newSoundName){};
+        SoundName(
+            SoundResource::soundName_e newSoundName = SoundResource::UNDEFINED, status_e status = status_e::STOPED)
+            : soundName(newSoundName), _status(status){};
 
         /// @brief Default destructor of SoundName component.
         ~SoundName() = default;
+
+        /// @brief This function return the status of the Sound
+        inline status_e getStatus() const { return _status; };
+
+        /// @brief This function set the status of the Sound
+        inline void setStatus(status_e status) { _status = status; };
+
+        /// @brief This function return the name of the Sound
+        inline SoundResource::soundName_e getName() const { return soundName; };
+
+        /// @brief This function set the name of the Sound
+        inline void setName(SoundResource::soundName_e name) { soundName = name; };
+
+      private:
+        status_e _status;
     };
 } // namespace ecs
-
 
 #endif /* !SOUNDNAME_HPP_ */
