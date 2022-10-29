c1fa2957fb27671f4f5a3581136d8ef3db8d6787
Bunny <tomduvalmahe44@gmail.com>
2022-10-29 16:48:53 +0200

    Merge 203 into 204 #204

--- libs/GraphicECS/SFML/Resources/SoundResource.cpp
+++ libs/GraphicECS/SFML/Resources/SoundResource.cpp
@@ -13,15 +13,10 @@ void SoundResource::addSound(const soundName_e sound_e, const std::filesystem::p
 {
     sf::SoundBuffer buffer;
     sf::Sound sound;
-<<<<<<< HEAD
-    
-    buffer.loadFromFile(soundPath);
-=======
 
     if (!buffer.loadFromFile(soundPath))
         // throw error
         void();
->>>>>>> 203-add-sound-in-r-type-game
     sound.setBuffer(buffer);
     _soundsList.emplace(sound_e, std::make_shared<sf::Sound>(sound));
 }
