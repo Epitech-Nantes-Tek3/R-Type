705e01f1116ab8088e5ee1051d19217240cbf739
Bunny <tomduvalmahe44@gmail.com>
2022-10-29 09:57:12 +0200

    Add Stop system for music #205

--- libs/GraphicECS/SFML/Systems/StopMusic.cpp
+++ libs/GraphicECS/SFML/Systems/StopMusic.cpp
@@ -12,17 +12,18 @@ using namespace graphicECS::SFML::Components;
 
 void StopMusic::run(World &world)
 {
-    std::vector<std::shared_ptr<ecs::Entity>> joined = world.joinEntities<Music>();
+    std::vector<std::shared_ptr<ecs::Entity>> joined = world.joinEntities<MusicName>();
 
-    auto makeParallax = [](std::shared_ptr<ecs::Entity> entityPtr) {
-        MusicName musicname = entityPtr.get()->getComponent<Music>();
-        MusicResource music = world.getResource<MusicResource>()
+    auto stopMusic = [world](std::shared_ptr<ecs::Entity> entityPtr) {
+        MusicName music = entityPtr.get()->getComponent<MusicName>(); // get Commponent
+        MusicResource &musicsList = world.getResource<MusicResource>(); // get all resources
+       
+        if (music.getStatus() != MusicName::STOPED)
+            music.setStatus(MusicName::STOPED);
 
-        if (musicname.getStatus() != MusicName::STOPED)
-            musicname.setStatus(STOPED);
-        // Recuperer la musique dans la shared Resource et la mettre en stoped
+        musicsList._musicsList.at(music.getName())->stop();
     };
 
-    std::for_each(joined.begin(), joined.end(), makeParallax);
+    std::for_each(joined.begin(), joined.end(), stopMusic);
 
 }
