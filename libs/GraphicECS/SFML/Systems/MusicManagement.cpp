4c2ab5508678078e0d3716899d2a30054a2b9f49
Bunny <tomduvalmahe44@gmail.com>
2022-10-29 13:48:00 +0200

    Add function to manage musics #206
    Pause, stop, play

--- libs/GraphicECS/SFML/Systems/MusicManagement.cpp
+++ libs/GraphicECS/SFML/Systems/MusicManagement.cpp
@@ -13,24 +13,22 @@ namespace graphicECS::SFML::Systems
 {
     void MusicManagement::run(World &world)
     {
-        void(world);
+        std::vector<std::shared_ptr<ecs::Entity>> joined = world.joinEntities<MusicName>();
+
+        auto stopMusic = [&world](std::shared_ptr<ecs::Entity> entityPtr) {
+            MusicName music = entityPtr.get()->getComponent<MusicName>();
+
+            if (music.getStatus() == MusicName::STOPED) {
+                world.getResource<MusicResource>()._musicsList.at(music.getName())->stop();
+            }
+            if (music.getStatus() == MusicName::PAUSED) {
+                world.getResource<MusicResource>()._musicsList.at(music.getName())->pause();
+            }
+            if (music.getStatus() == MusicName::PLAYING) {
+                world.getResource<MusicResource>()._musicsList.at(music.getName())->play();
+            }
+        };
+
+        std::for_each(joined.begin(), joined.end(), stopMusic);
     }
 } // namespace graphicECS::SFML::Systems
-
-        // std::vector<std::shared_ptr<ecs::Entity>> joined = world.joinEntities<MusicName>();
-
-        // auto stopMusic = [&world](std::shared_ptr<ecs::Entity> entityPtr) {
-        //     MusicName music = entityPtr.get()->getComponent<MusicName>();
-
-        //     if (music.getStatus() == MusicName::STOPED) {
-        //         world.getResource<MusicResource>()._musicsList.at(music.getName())->stop();
-        //     }
-        //     if (music.getStatus() == MusicName::PAUSED) {
-        //         world.getResource<MusicResource>()._musicsList.at(music.getName())->pause();
-        //     }
-        //     if (music.getStatus() == MusicName::PLAYING) {
-        //         world.getResource<MusicResource>()._musicsList.at(music.getName())->play();
-        //     }
-        // };
-
-        // std::for_each(joined.begin(), joined.end(), stopMusic);
