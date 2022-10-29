03463b0a8b636a28ad7590af79d97e52cfbee4e1
Bunny <tomduvalmahe44@gmail.com>
2022-10-29 13:05:33 +0200

    Add function run for MusicManagement in cpp #206
    Add a function to pause stop and play music

--- libs/GraphicECS/SFML/Systems/MusicManagement.cpp
+++ libs/GraphicECS/SFML/Systems/MusicManagement.cpp
@@ -4,3 +4,29 @@
 ** File description:
 ** MusicManagement
 */
+
+using namespace graphicECS::SFML::Components;
+
+namespace graphicECS::SFML::Systems
+{
+    void MusicManagement::run(World &world)
+    {
+        std::vector<std::shared_ptr<ecs::Entity>> joined = world.joinEntities<MusicName>();
+
+        auto stopMusic = [&world](std::shared_ptr<ecs::Entity> entityPtr) {
+            MusicName sound = entityPtr.get()->getComponent<MusicName>();
+
+            if (sound.getStatus() == MusicName::STOPED) {
+                world.getResource<MusicResource>()._soundsList.at(sound.getName())->stop();
+            }
+            if (sound.getStatus() == MusicName::PAUSED) {
+                world.getResource<MusicResource>()._soundsList.at(sound.getName())->pause();
+            }
+            if (sound.getStatus() == MusicName::PLAYING) {
+                world.getResource<MusicResource>()._soundsList.at(sound.getName())->play();
+            }
+        };
+
+        std::for_each(joined.begin(), joined.end(), stopMusic);
+    }
+} // namespace graphicECS::SFML::Systems
