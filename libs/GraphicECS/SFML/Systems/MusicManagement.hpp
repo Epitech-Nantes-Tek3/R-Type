44a16fae70518fed00b2ecdfbc4e6fe305062037
Bunny <tomduvalmahe44@gmail.com>
2022-10-29 12:58:54 +0200

    Add Music Management.hpp file #206

---
+++ libs/GraphicECS/SFML/Systems/MusicManagement.hpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2022
+** R-Type
+** File description:
+** MusicManagement
+*/
+
+#ifndef MUSICMANAGEMENT_HPP_
+#define MUSICMANAGEMENT_HPP_
+
+
+
+#endif /* !MUSICMANAGEMENT_HPP_ */
