fc418807c9ef42cd24b063440d05aa503390978b
Lucas Tesnier <lucas.tesnier@epitech.eu>
2022-10-31 14:32:59 +0100

    Add player name inside player component #275

--- libs/R-TypeLogic/Global/Components/PlayerComponent.hpp
+++ libs/R-TypeLogic/Global/Components/PlayerComponent.hpp
@@ -18,9 +18,12 @@ namespace ecs
         /// @brief Identification number for knowing wich player is
         unsigned short playerIdentifier;
 
+        /// @brief Name of the player
+        std::string name;
+
         /// @brief Construct a new player object
         /// @param playerIdentity Identification number of the player
-        Player(unsigned short playerIdentity = 0) : playerIdentifier(playerIdentity){};
+        Player(unsigned short playerIdentity = 0, std::string playerName = "") : playerIdentifier(playerIdentity), name(playerName){};
     };
 } // namespace ecs
 
