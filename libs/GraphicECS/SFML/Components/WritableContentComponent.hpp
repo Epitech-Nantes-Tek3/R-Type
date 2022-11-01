f5faa699b5cef253764c1d0e48632ee7cfbb1254
Lucas Tesnier <lucas.tesnier@epitech.eu>
2022-11-01 22:14:28 +0100

    Add content variable inside WritableContent component #288

--- libs/GraphicECS/SFML/Components/WritableContentComponent.hpp
+++ libs/GraphicECS/SFML/Components/WritableContentComponent.hpp
@@ -11,7 +11,17 @@
 namespace graphicECS::SFML::Components
 {
     /// @brief This component class stores the content of a writable component
-    class WritableContent : public ecs::Component {};
+    class WritableContent : public ecs::Component {
+
+        /// @brief A string containing all the typed content
+        std::string content(400, '\0');
+
+        /// @brief Default constructor of WritableContentComponent.
+        WritableContent() = default;
+
+        /// @brief Destroy the Writable Content object
+        ~WritableContent() = default;
+    };
 } // namespace graphicECS::SFML::Components
 
 #endif /* !WRITABLECONTENTCOMPONENT_HPP_ */
