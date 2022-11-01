e20867744a6bd2d63d04b92e86d56a8d77355026
Nathan <nathan.rousseau@epitech.eu>
2022-11-01 20:56:13 +0100

    Add new types for the enemies #277
    Fire, Electric, Ice and Basic

--- libs/R-TypeLogic/Global/Components/EnemyComponent.hpp
+++ libs/R-TypeLogic/Global/Components/EnemyComponent.hpp
@@ -13,7 +13,20 @@
 namespace ecs
 {
     /// @brief Enemy Component allows an entity to be used as an Enemy
-    class Enemy : public Component {};
+    class Enemy : public Component {
+        public:
+            /// @brief Enumeration of all current types of mob
+            enum type_e {BASIC, FIRE, ELECTRIC, ICE};
+
+            /// @brief The type of enemy, useful for the pattern and generated projectiles
+            unsigned short enemyType;
+
+            /// @brief Construct an Enemy with its type
+            Enemy(type_e newType = BASIC) : enemyType(newType) {};
+
+            /// @brief Default Destructor
+            ~Enemy() = default;
+    };
 } // namespace ecs
 
 #endif /* !ENEMYCOMPONENT_HPP_ */
