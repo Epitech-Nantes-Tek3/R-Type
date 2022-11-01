0684154f81d51d5504fb78e897738aa131ce7b14
Lucas Tesnier <lucas.tesnier@epitech.eu>
2022-11-01 21:52:22 +0100

    Implement a new component named WritableContent #288

---
+++ libs/GraphicECS/SFML/Components/WritableContentComponent.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2022
+** Project
+** File description:
+** WritableContentComponent
+*/
+
+#ifndef WRITABLECONTENTCOMPONENT_HPP_
+#define WRITABLECONTENTCOMPONENT_HPP_
+
+namespace graphicECS::SFML::Components
+{
+    /// @brief This component class stores the content of a writable component
+    class WritableContent : public ecs::Component {};
+} // namespace graphicECS::SFML::Components
+
+#endif /* !WRITABLECONTENTCOMPONENT_HPP_ */
