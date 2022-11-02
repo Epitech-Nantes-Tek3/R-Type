c25d3c1aadd8e5957dff338eb76221c278e83fc6
Nathan <nathan.rousseau@epitech.eu>
2022-11-02 19:57:09 +0100

    Generate a random type of enemy #276

--- Server/Room.cpp
+++ Server/Room.cpp
@@ -208,8 +208,14 @@ void Room::holdANewConnexionRequest(CommunicatorMessage connexionDemand)
     std::string playerNameStr = _getPlayerName(connexionDemand);
     std::size_t playerId = createNewPlayer(*_worldInstance.get(), 20, 500, 0, 0, 1, 102, 102, 100, 10, 4, false,
         _remainingPlaces + 1, playerNameStr, "", generator.generateNewNetworkableId());
+    // TO BE REMOVED WHEN TRUE MOB GENERATION WILL BE IMPLEMENTED
+    RandomDevice &random = _worldInstance->getResource<RandomDevice>();
+    random.lock();
+    /// Enemy::BASIC and Enemy::ICE
+    unsigned short randType = random.randInt(0, 3);
+    random.unlock();
     std::size_t enemyId = createNewEnemyRandom(
-        *_worldInstance.get(), 0, 0, 1, 85, 85, 50, 10, 5, Enemy::BASIC, "", generator.generateNewNetworkableId());
+        *_worldInstance.get(), 0, 0, 1, 85, 85, 50, 10, 5, randType, "", generator.generateNewNetworkableId());
     std::vector<std::shared_ptr<ecs::Entity>> clients = _worldInstance.get()->joinEntities<ecs::NetworkClient>();
     std::vector<unsigned short> clientIdList;
     auto addToClientList = [&clientIdList](std::shared_ptr<ecs::Entity> entityPtr) {
