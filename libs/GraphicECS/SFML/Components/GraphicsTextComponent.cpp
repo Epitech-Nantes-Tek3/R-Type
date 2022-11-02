e9830ca889e0fccba7ad2e1bea976b8e050b5c07
Lucas Tesnier <lucas.tesnier@epitech.eu>
2022-11-02 17:24:35 +0100

    Improve misunderstanding documentation #288

--- libs/GraphicECS/SFML/Components/GraphicsTextComponent.cpp
+++ libs/GraphicECS/SFML/Components/GraphicsTextComponent.cpp
@@ -12,11 +12,9 @@ using namespace graphicECS::SFML::Components;
 GraphicsTextComponent::GraphicsTextComponent(const sf::Font &newFont,
     const std::string newText, const std::size_t x, const std::size_t y, const std::size_t size)
 {
-    unsigned short textLen = 0;
     std::string formatedText = newText;
 
-    for (; newText[textLen] != '\0'; textLen++);
-    formatedText.resize(textLen);
+    formatedText.resize(formatedText.length());
     text.setString(formatedText);
     text.setPosition(x, y);
     text.setCharacterSize(size);
