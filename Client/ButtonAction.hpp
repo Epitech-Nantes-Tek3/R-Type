9f88ebfcc0c9386c0f15097ad5609760b8b128c4
Lucas Tesnier <lucas.tesnier@epitech.eu>
2022-11-02 17:19:11 +0100

    Improve misunderstanding documentation #288

--- Client/ButtonAction.hpp
+++ Client/ButtonAction.hpp
@@ -12,18 +12,22 @@
 
 /// @brief Exit the windows by disconecting the client
 /// @param world the world where the window will be closed
+/// @param entityPtr the entity whoe's actioned the called
 void exitWindow(World &world, Entity &entityPtr);
 
 /// @brief Pause the game, changing the menu state resource
 /// @param world the world where the game will be paused
+/// @param entityPtr the entity whoe's actioned the called
 void pauseGame(World &world, Entity &entityPtr);
 
 /// @brief Resume the game, changing the menu state resource
 /// @param world the world where the game will be resumed
+/// @param entityPtr the entity whoe's actioned the called
 void resumeGame(World &world, Entity &entityPtr);
 
 /// @brief Select a writable entity, add / remove a selected component
 /// @param world the world where the entity his
+/// @param entityPtr the entity whoe's actioned the called
 void selectAWritable(World &world, Entity &entityPtr);
 
 #endif /* !BUTTONACTION_HPP_ */
