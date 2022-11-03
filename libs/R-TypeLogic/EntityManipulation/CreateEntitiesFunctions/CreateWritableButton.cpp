553c61e105f571d05f7d5154eaf4bfbfe66f9508
Lucas Tesnier <lucas.tesnier@epitech.eu>
2022-11-03 13:50:53 +0100

    Add a new WritableButton entity #298

---
+++ libs/R-TypeLogic/EntityManipulation/CreateEntitiesFunctions/CreateWritableButton.cpp
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2022
+** rtype
+** File description:
+** CreateWritableButton
+*/
+
+#include "CreateWritableButton.hpp"
+#include <mutex>
+#include "R-TypeLogic/Global/Components/ButtonComponent.hpp"
+#include "R-TypeLogic/Global/Components/LayerLvL.hpp"
+#include "R-TypeLogic/Global/Components/PositionComponent.hpp"
+#include "R-TypeLogic/Global/Components/SizeComponent.hpp"
+
+namespace ecs
+{
+    std::size_t createNewWritableButton(World &world, const int posX, const int posY, const int sizeX, const int sizeY,
+        ButtonActionMap::buttonAction_e actionName, MenuStates::menuState_e state, std::size_t associatedWritableId)
+    {
+        Entity &entity = world.addEntity();
+        auto guard = std::lock_guard(entity);
+        entity.addComponent<Button>()
+            .addComponent<Size>(sizeX, sizeY)
+            .addComponent<Position>(posX, posY)
+            .addComponent<LayerLvL>(LayerLvL::WRITABLE_BUTTON)
+            .addComponent<ActionName>(actionName)
+            .addComponent<DisplayState>(state);
+        (void)associatedWritableId;
+        return entity.getId();
+    }
+} // namespace ecs
---
+++ libs/R-TypeLogic/EntityManipulation/CreateEntitiesFunctions/CreateWritableButton.hpp
@@ -0,0 +1,32 @@
+/*
+** EPITECH PROJECT, 2022
+** R-Type
+** File description:
+** CreateWritableButton
+*/
+
+#ifndef CREATEWRITABLEBUTTON_HPP_
+#define CREATEWRITABLEBUTTON_HPP_
+
+#include "World/World.hpp"
+#include "R-TypeLogic/EntityManipulation/ButtonManipulation/Components/ActionName.hpp"
+#include "R-TypeLogic/EntityManipulation/ButtonManipulation/Components/DisplayState.hpp"
+#include "R-TypeLogic/Global/Components/ButtonComponent.hpp"
+
+namespace ecs
+{
+    /// @brief This function creates a new WritableButton Entity with these parameters
+    /// @param world The world in which the WritableButton must be created
+    /// @param posX Position x of the WritableButton
+    /// @param posY Position y of the WritableButton
+    /// @param sizeX Size x of the WritableButton
+    /// @param sizeY Size y of the WritableButton
+    /// @param actionName The action name to be set for the WritableButton
+    /// @param displayState The associated state to decide when the writableButton will be displayed
+    /// @param associatedWritableId id of the associated writable entity
+    /// @return Id of the new WritableButton in std::size_t
+    std::size_t createNewWritableButton(World &world, const int posX, const int posY, const int sizeX, const int sizeY,
+        ButtonActionMap::buttonAction_e actionName, MenuStates::menuState_e state, std::size_t associatedWritableId);
+} // namespace ecs
+
+#endif /* !CREATEWRITABLEBUTTON_HPP_ */
