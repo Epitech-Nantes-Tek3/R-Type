2a5bf7a957f7d1c630d8569caab231fff31281e0
Lucas Tesnier <lucas.tesnier@epitech.eu>
2022-11-03 09:10:35 +0100

    Improve misunderstanding documentation #288

--- libs/R-TypeLogic/EntityManipulation/CreateEntitiesFunctions/CreateWritable.hpp
+++ libs/R-TypeLogic/EntityManipulation/CreateEntitiesFunctions/CreateWritable.hpp
@@ -19,7 +19,7 @@ namespace ecs
     /// @param posY Position y of the Writable
     /// @param sizeX Size x of the Writable
     /// @param sizeY Size y of the Writable
-    /// @param displayState The associated state to decide when the button will be displayed
+    /// @param state The associated state to decide when the writable will be displayed
     /// @return Id of the new Writable in std::size_t
     std::size_t createNewWritable(World &world, const int posX, const int posY, const int sizeX, const int sizeY, MenuStates::menuState_e state);
 } // namespace ecs
