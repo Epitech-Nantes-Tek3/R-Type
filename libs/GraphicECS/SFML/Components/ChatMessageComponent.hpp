68ce24ee1e5a91d96c27f86c6a065e36e80f5489
Lucas Tesnier <lucas.tesnier@epitech.eu>
2022-11-04 10:48:38 +0100

    Implement chatMessage component #301

---
+++ libs/GraphicECS/SFML/Components/ChatMessageComponent.hpp
@@ -0,0 +1,34 @@
+/*
+** EPITECH PROJECT, 2022
+** Project
+** File description:
+** ChatMessageComponent
+*/
+
+#ifndef CHATMESSAGECOMPONENT_HPP_
+#define CHATMESSAGECOMPONENT_HPP_
+
+namespace graphicECS::SFML::Components
+{
+    /// @brief This component class stores the content of a chat message component
+    class ChatMessage : public ecs::Component {
+      public:
+        /// @brief author of the message
+        std::string author;
+
+        /// @brief Content of the message
+        std::string message;
+
+        /// @brief Constructor of ChatMessage object.
+        inline ChatMessage()
+        {
+            author = "";
+            message = "";
+        };
+
+        /// @brief Destroy the Writable Content object
+        ~ChatMessage() = default;
+    };
+} // namespace graphicECS::SFML::Components
+
+#endif /* !CHATMESSAGECOMPONENT_HPP_ */
