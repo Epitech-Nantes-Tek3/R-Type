4ff11957ca48f759483f5bbf10805951734ef6a8
Bunny <tomduvalmahe44@gmail.com>
2022-11-05 19:08:19 +0100

    Add verification to shoot system #255
    Add a verification to checkif the world
    is in solo or multiplayer mod

--- libs/R-TypeLogic/Server/Systems/EnemyShootSystem.cpp
+++ libs/R-TypeLogic/Server/Systems/EnemyShootSystem.cpp
@@ -7,19 +7,19 @@
 
 #include "EnemyShootSystem.hpp"
 #include <chrono>
+#include <iostream>
 #include <mutex>
 #include "Transisthor/TransisthorECSLogic/Server/Resources/NetworkableIdGenerator.hpp"
 #include "R-TypeLogic/EntityManipulation/CreateEntitiesFunctions/CreateEnemyProjectile.hpp"
 #include "R-TypeLogic/Global/Components/EnemyComponent.hpp"
 #include "R-TypeLogic/Global/Components/ShootingFrequencyComponent.hpp"
 #include "R-TypeLogic/Global/SharedResources/GameClock.hpp"
-#include <iostream>
 using namespace ecs;
 using namespace std::chrono;
 
 void EnemyShootSystem::run(World &world)
 {
-     std::cout << "test system enemyshoot\n";
+    std::cout << "test system enemyshoot\n";
     std::vector<std::shared_ptr<ecs::Entity>> joined = world.joinEntities<Enemy, ShootingFrequency>();
 
     auto enemiesMayShoot = [&world](std::shared_ptr<ecs::Entity> entityPtr) {
@@ -27,13 +27,18 @@ void EnemyShootSystem::run(World &world)
         ShootingFrequency &freq = entityPtr.get()->getComponent<ShootingFrequency>();
 
         if (freq.frequency == duration<double>(0)) {
-            NetworkableIdGenerator &generator = world.getResource<NetworkableIdGenerator>();
-            auto guard = std::lock_guard(generator);
-            createNewEnemyProjectile(world, entityPtr, "", generator.generateNewNetworkableId());
-            freq.frequency = freq.baseFrequency;
-        }
-    };
-    std::cout << "test system enemyshoot mid\n";
-    std::for_each(joined.begin(), joined.end(), enemiesMayShoot);
-    std::cout << "test system enemyshoot end\n";
+            if (world.containsResource<NetworkableIdGenerator>()) {
+                NetworkableIdGenerator &generator = world.getResource<NetworkableIdGenerator>();
+                auto guard = std::lock_guard(generator);
+                createNewEnemyProjectile(world, entityPtr, "", generator.generateNewNetworkableId());
+                freq.frequency = freq.baseFrequency;
+            } else {
+                createNewEnemyProjectile(world, entityPtr, "");
+                freq.frequency = freq.baseFrequency;
+            }
+    }
+};
+std::cout << "test system enemyshoot mid\n";
+std::for_each(joined.begin(), joined.end(), enemiesMayShoot);
+std::cout << "test system enemyshoot end\n";
 }
