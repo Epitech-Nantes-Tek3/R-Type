5e1b3701dc614f2e2371263ceaf64587aeafafd1
osvegn <thomas.prudhomme@epitech.eu>
2022-11-07 20:16:37 +0100

    Fix clang format #316

--- Client/ButtonAction.cpp
+++ Client/ButtonAction.cpp
@@ -127,7 +127,8 @@ void connectToARoom(World &world, Entity &entityPtr)
     if (!world.containsResource<MenuStates>())
         return;
     short configs[6] = {120, 121, 122, 123, 124, 125};
-    world.getTransisthorBridge()->getCommunicatorInstance().utilitarySendRoomConfiguration(
-        "default000", configs, world.getTransisthorBridge()->getCommunicatorInstance().getClientByHisId(0)); /// WILL BE UPDATE WITH THE LOBBY MENU
+    world.getTransisthorBridge()->getCommunicatorInstance().utilitarySendRoomConfiguration("default000", configs,
+        world.getTransisthorBridge()->getCommunicatorInstance().getClientByHisId(
+            0)); /// WILL BE UPDATE WITH THE LOBBY MENU
     world.getResource<MenuStates>().currentState = MenuStates::LOBBY;
 }
