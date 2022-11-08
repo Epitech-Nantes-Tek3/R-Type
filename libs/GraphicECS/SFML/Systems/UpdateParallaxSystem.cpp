528451d457897b5ef5215ace53d24da7be45425c
Nathan <nathan.rousseau@epitech.eu>
2022-11-08 14:01:57 +0100

    Add conditions to change the background with the currLvl of the game #280

--- libs/GraphicECS/SFML/Systems/UpdateParallaxSystem.cpp
+++ libs/GraphicECS/SFML/Systems/UpdateParallaxSystem.cpp
@@ -6,10 +6,25 @@
 */
 
 #include "UpdateParallaxSystem.hpp"
+#include "R-TypeLogic/Global/SharedResources/GameLevel.hpp"
 
 using namespace graphicsECS::SFML::Systems;
 
 void UpdateParallax::run(World &world)
 {
-    (void)world;
+    if (!world.containsResource<GameLevel>())
+        return;
+    GameLevel &gameLvl = world.getResource<GameLevel>();
+
+    gameLvl.lock();
+    GameLevel::level_e currLvl = gameLvl.getCurrentLevel();
+    gameLvl.unlock();
+
+    switch (currLvl) {
+        case GameLevel::LEVEL_TWO: break;
+        case GameLevel::LEVEL_THREE: break;
+        case GameLevel::LEVEL_FORTH: break;
+        case GameLevel::LEVEL_INFINITE: break;
+        default: break;
+    };
 }
