dd217837e6ca4f412e26438d69554a25d0e59f2d
Nathan <nathan.rousseau@epitech.eu>
2022-11-08 13:44:12 +0100

    Add the Update Parallax System #280

---
+++ libs/GraphicECS/SFML/Systems/UpdateParallaxSystem.cpp
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2022
+** Project
+** File description:
+** UpdateParallaxSystem
+*/
+
+#include "UpdateParallaxSystem.hpp"
+
+using namespace graphicsECS::SFML::Systems;
+
+void UpdateParallax::run(World &world)
+{
+    (void)world;
+}
