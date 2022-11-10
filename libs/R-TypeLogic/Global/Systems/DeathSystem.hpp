ce1577815235450c4fea2912c8909a5c94dba0cc
Nathan <nathan.rousseau@epitech.eu>
2022-11-10 02:21:30 +0100

    Update the score when a enemy is dead #389

--- libs/R-TypeLogic/Global/Systems/DeathSystem.hpp
+++ libs/R-TypeLogic/Global/Systems/DeathSystem.hpp
@@ -15,10 +15,10 @@
 #include "R-TypeLogic/Global/Components/DeathComponent.hpp"
 #include "R-TypeLogic/Global/Components/EnemyComponent.hpp"
 #include "R-TypeLogic/Global/SharedResources/GameLevel.hpp"
+#include "R-TypeLogic/Global/SharedResources/GlobalScore.hpp"
 
 namespace ecs
-{
-    /// @brief This is the Death System. This system takes all entities which have Dead Component and
+{    /// @brief This is the Death System. This system takes all entities which have Dead Component and
     /// removes them from the world.
     /// It's a system that will be called by the engine world with the function runSystem()
     struct DeathSystem : public System {
@@ -29,18 +29,26 @@ namespace ecs
             auto death = [&world](std::shared_ptr<ecs::Entity> entityPtr) {
                 auto guard = std::lock_guard(*entityPtr.get());
                 if (entityPtr.get()->getComponent<Death>().modified != true) {
-                    if (entityPtr->contains<Enemy>() && world.containsResource<GameLevel>()) {
-                        GameLevel &gameLevel = world.getResource<GameLevel>();
-                        gameLevel.lock();
-                        gameLevel.addNewKills();
-                        gameLevel.unlock();
+                    if (entityPtr->contains<Enemy>()) {
+                        if (world.containsResource<GameLevel>()) {
+                            GameLevel &gameLevel = world.getResource<GameLevel>();
+                            gameLevel.lock();
+                            gameLevel.addNewKills();
+                            gameLevel.unlock();
+                        }
+                        if (world.containsResource<GlobalScore>()) {
+                            GlobalScore &gbScore = world.getResource<GlobalScore>();
+                            gbScore.lock();
+                            gbScore.updateScore(entityPtr->getComponent<Enemy>().enemyType);
+                            gbScore.unlock();
+                        }
                     }
-                    world.removeEntity(entityPtr->getId());
                 }
+                world.removeEntity(entityPtr->getId());
             };
             std::for_each(joined.begin(), joined.end(), death);
         }
     };
-} // namespace ecs
+}// namespace ecs
 
 #endif /* !DEATHSYSTEM_HPP_ */
