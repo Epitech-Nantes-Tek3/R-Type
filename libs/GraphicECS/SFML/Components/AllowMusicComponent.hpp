9aa1ddf25e438326eee33c536df2aa94f3036aea
osvegn <thomas.prudhomme@epitech.eu>
2022-11-11 09:51:42 +0100

    Create music and sound controller components #361

---
+++ libs/GraphicECS/SFML/Components/AllowMusicComponent.hpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2022
+** rtype
+** File description:
+** AllowMusicComponent
+*/
+
+#ifndef ALLOWMUSICCOMPONENT_HPP_
+#define ALLOWMUSICCOMPONENT_HPP_
+
+#include "Component/Component.hpp"
+
+namespace graphicECS::SFML::Components
+{
+    class AllowMusicComponent : public ecs::Component {};
+} // namespace graphicECS::SFML::Components
+
+#endif /* !ALLOWMUSICCOMPONENT_HPP_ */
---
+++ libs/GraphicECS/SFML/Components/AllowSoundComponent.hpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2022
+** rtype
+** File description:
+** AllowSoundComponent
+*/
+
+#ifndef ALLOWSOUNDCOMPONENT_HPP_
+#define ALLOWSOUNDCOMPONENT_HPP_
+
+#include "Component/Component.hpp"
+
+namespace graphicECS::SFML::Components
+{
+    class AllowSoundComponent : public ecs::Component {};
+} // namespace graphicECS::SFML::Components
+
+#endif /* !ALLOWSOUNDCOMPONENT_HPP_ */
