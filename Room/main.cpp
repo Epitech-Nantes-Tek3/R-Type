8c2175574a17ebb41c4874d53559d07572b48ecc
Lucas Tesnier <lucas.tesnier@epitech.eu>
2022-11-13 22:31:14 +0100

    Fix room configuration #404

--- Room/main.cpp
+++ Room/main.cpp
@@ -7,20 +7,17 @@
 
 #include <iostream>
 #include "ArgumentHandler/ArgumentHandler.hpp"
+#include "Error/Error.hpp"
 #include "Room.hpp"
 
+using namespace error_lib;
 using namespace argument_handler;
 
 int main(int ac, char **av)
 {
     ArgumentHandler argumentHandler = ArgumentHandler(ac, av);
     ArgumentHandler::RoomInformation roomInformations;
-    try {
-        roomInformations = argumentHandler.extractRoomInformations();
-    } catch (std::exception &exc) {
-        std::cerr << exc.what() << std::endl;
-        return 84;
-    }
+    roomInformations = argumentHandler.extractRoomInformations();
     server_data::Room room(roomInformations.id, roomInformations.name,
         Client(roomInformations.address, roomInformations.port), roomInformations.configs);
     room.startLobbyLoop();
