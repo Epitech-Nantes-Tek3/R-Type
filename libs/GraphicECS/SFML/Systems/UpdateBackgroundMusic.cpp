28ce8e8f33c4dac07fdd9e191e091d8e82b00800
Nathan <nathan.rousseau@epitech.eu>
2022-11-13 15:48:02 +0100

    Add Update Background music system #408

---
+++ libs/GraphicECS/SFML/Systems/UpdateBackgroundMusic.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2022
+** Project
+** File description:
+** UpdateBackgroundMusic
+*/
+
+#include "UpdateBackgroundMusic.hpp"
+
+using namespace graphicECS::SFML::Systems;
+
+void UpdateBackgroundMusic::run(World &world)
+{
+    (void)world;
+}
+
